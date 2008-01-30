6f5b8d4cda59adf51620f9f29ca0b8735bcb062a
BenTatham <BenTatham@05e6dff8-d244-0410-89c8-217543e244fc>
2008-01-30 04:42:59 +0000

    cleanup

--- trunk/src/main/java/ca/carleton/sysc5801/sim4j/NetworkException.java
+++ trunk/src/main/java/ca/carleton/sysc5801/sim4j/NetworkException.java
@@ -6,25 +6,25 @@ public class NetworkException extends Exception
   public NetworkException()
   {
     super();
-    // TODO Auto-generated constructor stub
+
   }
 
   public NetworkException(String message, Throwable cause)
   {
     super(message, cause);
-    // TODO Auto-generated constructor stub
+
   }
 
   public NetworkException(String message)
   {
     super(message);
-    // TODO Auto-generated constructor stub
+
   }
 
   public NetworkException(Throwable cause)
   {
     super(cause);
-    // TODO Auto-generated constructor stub
+
   }
 
 }
