ecf7dda7375ebd3bbd93213f4752512c5b669fea
BenTatham <BenTatham@05e6dff8-d244-0410-89c8-217543e244fc>
2008-02-05 04:26:18 +0000

    Send Packets

--- src/main/java/ca/carleton/sysc5801/sim4j/Node.java
+++ src/main/java/ca/carleton/sysc5801/sim4j/Node.java
@@ -2,12 +2,18 @@ package ca.carleton.sysc5801.sim4j;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Node
 {
   private final int m_id;
-  private final Collection<Link> m_links = new HashSet<Link>();
+  private final Map<Node, Link> m_links = new HashMap<Node, Link>();
+  private final Map<Node, Path> m_routes = new HashMap<Node, Path>();
+
+  /** infinite queue, with 1ms delay */
+  private final PacketQueue m_packetQueue =
+      new PacketQueue(new NodePacketProcessor(), Double.MAX_VALUE, 0.001d);
 
   Node(int id)
   {
@@ -19,14 +25,59 @@ public class Node
     return m_id;
   }
 
+  public void setRoute(Node destination, Path route)
+  {
+    m_routes.put(destination, route);
+  }
+
+  /**
+   * Send a new packet from this node as the source
+   * 
+   * @param packet
+   */
+  public void sendPacket(Packet packet)
+  {
+    Path path = m_routes.get(packet.getDestination());
+    packet.setPath(path.getPath());
+    forward(packet);
+  }
+
+  /**
+   * Begin processing this packet and forward it to the next node in the
+   * packet's path after the processing delay
+   * 
+   * @param packet
+   */
+  public void forward(Packet packet)
+  {
+    if (packet.getDestination().equals(this))
+    {
+      System.out.println("Received packet from: " + packet.getSource());
+    }
+    else
+    {
+      m_packetQueue.offer(packet);
+    }
+  }
+
+  public void tick(double timeIncrement)
+  {
+    m_packetQueue.tick(timeIncrement);
+  }
+
   public void addLink(Link link)
   {
-    m_links.add(link);
+    m_links.put(link.getOther(this), link);
   }
 
   public Collection<Link> getLinks()
   {
-    return m_links;
+    return m_links.values();
+  }
+
+  public Link getLink(Node other)
+  {
+    return m_links.get(other);
   }
 
   public Collection<Node> getNeighbors()
@@ -64,4 +115,19 @@ public class Node
   {
     return "Node " + getId();
   }
+
+  private final class NodePacketProcessor implements
+      PacketQueue.PacketProcessor
+  {
+    /**
+     * Forward the packet to the next stop on its path.
+     */
+    @Override
+    public void process(Packet packet)
+    {
+      Link link = getLink(packet.getNextStop());
+      link.send(packet);
+    }
+  }
+
 }
---
+++ src/main/java/ca/carleton/sysc5801/sim4j/PacketQueue.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package ca.carleton.sysc5801.sim4j;
+
+import java.util.LinkedList;
+
+class PacketQueue
+{
+  private final double m_capacity;
+  private final double m_delay;
+
+  private int m_current = 0;
+  private final LinkedList<PacketQueue.PropogatingPacket> m_packets =
+      new LinkedList<PacketQueue.PropogatingPacket>();
+  private final PacketProcessor m_processor;
+
+  PacketQueue(PacketProcessor processor, double capacity, double delay)
+  {
+    m_processor = processor;
+    m_capacity = capacity;
+    m_delay = delay;
+  }
+
+  double getDelay()
+  {
+    return m_delay;
+  }
+
+  boolean offer(Packet packet)
+  {
+    if (m_current + packet.getSize() < m_capacity)
+    {
+      m_current += packet.getSize();
+      m_packets.add(new PacketQueue.PropogatingPacket(packet, getDelay()));
+      return true;
+    }
+    return false;
+  }
+
+  void tick(double seconds)
+  {
+    for (PacketQueue.PropogatingPacket waitingPacket : m_packets)
+    {
+      waitingPacket.timeLeft -= seconds;
+      if (waitingPacket.timeLeft <= 0)
+      {
+        Packet packet = waitingPacket.packet;
+        m_current -= packet.getSize();
+        m_processor.process(packet);
+      }
+    }
+  }
+
+  static class PropogatingPacket
+  {
+    Packet packet;
+    double timeLeft;
+
+    PropogatingPacket(Packet aPacket, double time)
+    {
+      packet = aPacket;
+      timeLeft = time;
+    }
+  }
+
+  public static interface PacketProcessor
+  {
+    void process(Packet packet);
+  }
+
+}
