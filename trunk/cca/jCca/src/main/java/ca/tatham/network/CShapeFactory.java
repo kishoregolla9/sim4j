ec0a193c6977d4b8d0ed3daf46465b33a520e047
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2008-07-08 01:54:23 +0000

    initial import

---
+++ trunk/cca/jCca/src/main/java/ca/tatham/network/CShapeFactory.java
@@ -0,0 +1,38 @@
+package ca.tatham.network;
+
+public class CShapeFactory
+{
+  public static Node[] getGrid(int size)
+  {
+    Node[] network = new Node[size];
+    double fraction = 0.3d;
+    int index = 0;
+    for (int i = 0; i < size; i++)
+    {
+      if (i < size * fraction)
+      {
+        for (int j = 0; j < size; j++)
+        {
+          network[index] = new Node(i + (Math.random() - 0.5) * 0.4, j
+              + (Math.random() - 0.5) * 0.4);
+          index++;
+        }
+      } else
+      {
+        for (int j = 0; j < size * fraction; j++)
+        {
+          network[index] = new Node(i + (Math.random() - 0.5) * 0.4, j
+              + (Math.random() - 0.5) * 0.4);
+          index++;
+        }
+        for (int j = (int) (size * (1 - fraction)); j < size; j++)
+        {
+          network[index] = new Node(i + (Math.random() - 0.5) * 0.4, j
+              + (Math.random() - 0.5) * 0.4);
+          index++;
+        }
+      }
+    }
+    return network;
+  }
+}
