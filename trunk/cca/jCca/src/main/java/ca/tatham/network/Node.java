6468571b5d6f22adba3f6761c2e217876057325b
BenTatham <BenTatham@05e6dff8-d244-0410-89c8-217543e244fc>
2008-08-28 00:44:39 +0000

    where I left off

--- trunk/cca/jCca/src/main/java/ca/tatham/network/Node.java
+++ trunk/cca/jCca/src/main/java/ca/tatham/network/Node.java
@@ -1,9 +1,13 @@
 package ca.tatham.network;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Node
 {
   private double m_x;
   private double m_y;
+  private Map<Double, Double> m_connectivity = new HashMap<Double, Double>();
 
   public Node(double x, double y)
   {
@@ -19,6 +23,16 @@ public class Node
   public double y()
   {
     return m_y;
+  }
+
+  public void setConnectivity(double radius, double connectivity)
+  {
+    m_connectivity.put(radius, connectivity);
+  }
+
+  public double getConnectivity(double radius)
+  {
+    return m_connectivity.get(radius);
   }
 
 }
