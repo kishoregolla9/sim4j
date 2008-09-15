9ef60e67e0543cd4b846f77d784ce5a343ef5e4a
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2008-09-15 02:19:14 +0000

    Floyd algorithm

---
+++ cca/jCca/src/test/java/ca/tatham/network/NodeTest.java
@@ -0,0 +1,13 @@
+package ca.tatham.network;
+
+import junit.framework.TestCase;
+
+public class NodeTest extends TestCase
+{
+  public void testShortedDistanceMatrix()
+  {
+    Node[] nodes = new Node[10];
+
+    Network network = new Network(nodes, 5);
+  }
+}
