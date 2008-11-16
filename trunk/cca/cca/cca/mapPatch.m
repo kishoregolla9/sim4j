6fd9779305d95963b8e1b5ebea85f489ee976c5f
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2008-11-16 13:37:50 +0000

    more name changes

--- trunk/cca/cca/cca/mapPatch.m
+++ trunk/cca/cca/cca/mapPatch.m
@@ -1,43 +1,48 @@
-function [patchTime,coordinates_median,coordinates_median_average,allResults]=mapPatch(network, radiusNet,startNode,anchor,CL_all)
+function [patchTime,coordinates_median,coordinates_median_average,allResults]=...
+    mapPatch(network,localMaps,startNodes,anchorSets)
 %This function patches local maps into the global map for all the local maps
-%computed for each radius value stored in the radiusNet.
+%computed for each radius value stored in the localMaps.
 %input:
-%network - Nx2 matrix of the network
-%radiusNet - computed using localMapComputing.m
-%startNode - 1xM matrix containing M starting nodes that want to be
-%experimented with
-%anchor - SxT matrix containing S anchor sets that want to be experimented
-%with. Each anchor set has T anchor nodes. 
+%  network - Nx2 matrix of the network
+%  localMaps - computed using localMapComputing.m
+%  startNodes - 1xM matrix containing M starting nodes that want to be
+%    experimented with
+%  anchorSets - SxT matrix containing S anchorSets sets that want to be experimented
+%    with. Each anchorSets set has T anchorSets nodes.
 
-node=startNode
+node=startNodes
+points=network.points;
+numStartNodes=size(startNodes,2);%number of starting nodes
+numAnchorSets=size(anchorSets,1); %nubmer of anchorSets sets for testing
+numRadiusLevels=size(localMaps,2); %number of radius levels computed
 
-sn=size(startNode,2);%number of starting nodes   
-an=size(anchor,2); %number of anchor nodes
-asn=size(anchor,1); %nubmer of anchor sets for testing
-cl=size(radiusNet,2); %number of radius levels computed
+for startNodeIndex=1:numStartNodes % for each starting node
+    sprintf('++++ Starting Node %i', startNodeIndex)
+    for radiusIndex=1:numRadiusLevels %for each radius level
+        sprintf('++++ Radius Level %i', radiusIndex)
+        A=zeros(numAnchorSets,1);
+        times=zeros(numAnchorSets,1);
+        for anchorSetIndex=1:numAnchorSets % for each anchorSets set
+            sprintf('++++ Anchor Set %i', anchorSetIndex)
+            %       localMaps{radiusIndex}(1).radius=connectivityLevels(1,radiusIndex);
+            localMaps{radiusIndex}= ...
+                mapVitPatch(points,localMaps{radiusIndex},node(startNodeIndex),...
+                anchorSets(anchorSetIndex,:),localMaps{radiusIndex}(1).radius);
+            localMaps{radiusIndex}(node(startNodeIndex))
+            A(anchorSetIndex)=...
+                (localMaps{radiusIndex}(node(startNodeIndex)).patched_net_coordinates_error_median(1)+...,
+                localMaps{radiusIndex}(node(startNodeIndex)).patched_net_coordinates_error_median(2))/2
+            times(anchorSetIndex)=localMaps{radiusIndex}(node(startNodeIndex)).map_patchTime;
+        end
 
-for jj=1:sn % for each starting node
-  disp('++++ Starting Node ' + jj)
-  for ii=1:cl %for each radius level
-    disp('++++ Radius Level ' + ii)
-    for kk=1:asn % for each anchor set
-        disp('++++ Anchor Node ' + kk)
-%       radiusNet{ii}(1).radius=CL_all(1,ii);
-        radiusNet{ii}=mapVitPatch(network,radiusNet{ii},node(jj),anchor(kk,:),radiusNet{ii}(1).radius)
-        radiusNet{ii}(node(jj))
-        A(kk)=(radiusNet{ii}(node(jj)).patched_net_coordinates_error_median(1)+...,
-            radiusNet{ii}(node(jj)).patched_net_coordinates_error_median(2))/2
-        T(kk)=radiusNet{ii}(node(jj)).map_patchTime;
-    end
+        allResults(radiusIndex,:,startNodeIndex)=A;
+        patchTime(startNodeIndex,radiusIndex)=median(times')
+        % A=A(1:3);
+        coordinates_median(startNodeIndex,radiusIndex)=median(A')
+        coordinates_median_average(startNodeIndex,radiusIndex)=mean(A')
 
-    allResults(ii,:,jj)=A;
-    patchTime(jj,ii)=median(T')
-    % A=A(1:3);
-    coordinates_median(jj,ii)=median(A')
-    coordinates_median_average(jj,ii)=mean(A')
-
-    clear A;
-    clear T;
-  end %for ii
-end %for jj 
+        clear A;
+        clear times;
+    end % for radiusIndex
+end % for startNodeIndex
 
