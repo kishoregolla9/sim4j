3635d909fb10bf0f8f0bc40062bb5539ba439bbc
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2008-11-16 17:40:50 +0000

    format

--- cca/cca/cca/mapVitPatch.m
+++ cca/cca/cca/mapVitPatch.m
@@ -1,62 +1,62 @@
 function [node]=mapVitPatch(network,node,node_k,anchorNodes,r)
 %function mapVitPatch takes the input of local maps and patch them together
-%into a global map and compare the results with the original 'Network' 
-%taking the map of node_k as the starting map. 
+%into a global map and compare the results with the original 'Network'
+%taking the map of node_k as the starting map.
 % The %input 'node' should be generated from the function of
 % localMapLocalization.m or equivalent
 %option - Use the greedy patch taking the node that has the most number of
 %commone nodes for patch when 'option' is given as '1'. When 'option' is
 %'0', patch the map as fast as we can by taking the node that has most
-%number of different nodes. 
+%number of different nodes.
 
-    points = network.points;
-    if ( isstruct(network) && isfield(network,'distanceMatrix') )
-      distanceMatrix=network.distanceMatrix;
-    else
-      sprintf('network.distanceMatrix does not exist');        
-      distanceMatrix=sqrt(disteusq(network.points,network.points,'x'));
-      network.distanceMatrix=distanceMatrix;
-    end
+points = network.points;
+if ( isstruct(network) && isfield(network,'distanceMatrix') )
+    distanceMatrix=network.distanceMatrix;
+else
+    sprintf('network.distanceMatrix does not exist');
+    distanceMatrix=sqrt(disteusq(network.points,network.points,'x'));
+    network.distanceMatrix=distanceMatrix;
+end
 
-    N=network.numberOfNodes;
-	for ii=1:N      
-        map{ii}=node(ii).local_network_c; %added by li as cca generates directly the network
-        index{ii} = (node(ii).neighbors_merge)'; %grab all the nodes in the local map
-        indexInclude{ii} = ii;
-    end %for ii
-    
-    [D_hopDist, ConnectivityM]=createDistanceMatrix(network,r);
-	tStart = cputime;
-	
+N=network.numberOfNodes;
+for ii=1:N
+    map{ii}=node(ii).local_network_c; %added by li as cca generates directly the network
+    index{ii} = (node(ii).neighbors_merge)'; %grab all the nodes in the local map
+    indexInclude{ii} = ii;
+end %for ii
+
+[shortestHopMatrix, ConnectivityM]=createDistanceMatrix(network,r);
+tStart = cputime;
+
 % 	curNode = ceil(rand*N); %randomly select the starting node
-    curNode=node_k;
-    curMap = map{curNode};
-    curindex = index{curNode};
-    curindexInclude = indexInclude{curNode};
+curNode=node_k;
+curMap = map{curNode};
+curindex = index{curNode};
+curindexInclude = indexInclude{curNode};
 
-	while length(curindexInclude) ~= N
-        nodeList = setdiff(curindex, curindexInclude);
-        
-        nxlist = zeros(length(nodeList),1);
-        for i=1:length(nodeList)
-            node2 = nodeList(i);
-            nxlist(i) = length(intersect(curindex,index{node2}));
-        end
-        [tmp, j] = max(nxlist);
-        
-        node2 = nodeList(j); % find the node with maximum intersection with 
-        [curMap, curindex, curindexInclude] = mergeMap(...
-                curMap, map{node2}, curindex, index{node2}, ...
-                curindexInclude, indexInclude{node2},ConnectivityM);
-    end %while
-	tEnd = cputime;
-	disp(['Patching the local maps takes ' num2str(tEnd-tStart) ' sec']);
-    node(node_k).map_patchTime=tEnd-tStart;
-	%%
-	rawResult = curMap;
-    node(node_k).patched_network=rawResult;
-    refineResult=rawResult; %no refinement
-    
+while length(curindexInclude) ~= N
+    nodeList = setdiff(curindex, curindexInclude);
+
+    nxlist = zeros(length(nodeList),1);
+    for i=1:length(nodeList)
+        node2 = nodeList(i);
+        nxlist(i) = length(intersect(curindex,index{node2}));
+    end
+    [tmp, j] = max(nxlist);
+
+    node2 = nodeList(j); % find the node with maximum intersection with
+    [curMap, curindex, curindexInclude] = mergeMap(...
+        curMap, map{node2}, curindex, index{node2}, ...
+        curindexInclude, indexInclude{node2},ConnectivityM);
+end %while
+tEnd = cputime;
+disp(['Patching the local maps takes ' num2str(tEnd-tStart) ' sec']);
+node(node_k).map_patchTime=tEnd-tStart;
+%%
+rawResult = curMap;
+node(node_k).patched_network=rawResult;
+refineResult=rawResult; %no refinement
+
 
 knownJ=anchorNodes;
 
@@ -66,20 +66,20 @@ mappedResult = TRANSFORM.b * refineResult(:,1:2) * TRANSFORM.T + ...
 
 % set output
 % t.xyEstimate = mappedResult;
-    node(node_k).patched_network_transform=mappedResult;
-    D_C = sqrt(disteusq(mappedResult,mappedResult,'x'));
-    D_dist_mean = mean((mean(abs(D_C-distanceMatrix)))');
-    D_dist_mean=D_dist_mean/r;
-    D_coordinates_mean=mean(abs(mappedResult-network));
-    D_coordinates_mean=D_coordinates_mean/r;
-    node(node_k).patched_net_dist_error_mean=D_dist_mean;
-    node(node_k).patched_net_coordinates_error_mean=D_coordinates_mean;
-    D_coordinates_median=median(abs(mappedResult-network));
-    D_coordinates_median=D_coordinates_median/r;
-    node(node_k).patched_net_coordinates_error_median=D_coordinates_median;
+node(node_k).patched_network_transform=mappedResult;
+D_C = sqrt(disteusq(mappedResult,mappedResult,'x'));
+D_dist_mean = mean((mean(abs(D_C-distanceMatrix)))');
+D_dist_mean=D_dist_mean/r;
+D_coordinates_mean=mean(abs(mappedResult-network));
+D_coordinates_mean=D_coordinates_mean/r;
+node(node_k).patched_net_dist_error_mean=D_dist_mean;
+node(node_k).patched_net_coordinates_error_mean=D_coordinates_mean;
+D_coordinates_median=median(abs(mappedResult-network));
+D_coordinates_median=D_coordinates_median/r;
+node(node_k).patched_net_coordinates_error_median=D_coordinates_median;
 
 
-return;  
+return;
 
 
 %%
@@ -89,7 +89,7 @@ return;
 %% function [newMap, newIndex, newIndexInclude] = mergeMap(map1, map2, ...
 %%    index1, index2, indexInclude1, indexInclude2)
 %%
-%% Input: 
+%% Input:
 %%      map1 - local map of n1
 %%      map2 - local map of n2
 %%      index1 - indices of the nodes in n1' map
@@ -98,9 +98,9 @@ return;
 %           in n1' map
 %%      indexInclude2 - indices of the nodes whose maps are contained
 %           in n2' map
-%% Output: 
+%% Output:
 %%      newMap - the merged map
-%%      newIndex - indices of the nodes in the new map (sorted ascending) 
+%%      newIndex - indices of the nodes in the new map (sorted ascending)
 %%      newIndexInclude - indices of the nodes whose maps have been contained
 %           in the new map (sorted ascending)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -118,20 +118,20 @@ unionIndex = union(index1, index2);
 [D, Z, TRANSFORM] = procrustes(map1(ii1,:), map2(ii2,:));
 
 % if method == 1
-    % -- Method 1: scale and average
-    %%%%%%%%%%%%%%%%%%%%%% used this block for range based. usually not good %%%%%%%%%%%%%
+% -- Method 1: scale and average
+%%%%%%%%%%%%%%%%%%%%%% used this block for range based. usually not good %%%%%%%%%%%%%
 %     newMap2 = TRANSFORM.b * map2 * TRANSFORM.T + ...
 %         repmat(TRANSFORM.c(1,:),length(index2),1);
-%     newMap = map1; 
+%     newMap = map1;
 %     newMap(ii1,:) = 0.5 *(map1(ii1,:) + Z);
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % elseif method == 2
 %     -- Method 2: average (do not scale)- better one
-% 
-    newMap2 = map2 * TRANSFORM.T + ...
-        repmat(TRANSFORM.c(1,:),length(index2),1);
-    newMap = map1; 
-    newMap(ii1,:) = 0.5 *(map1(ii1,:) + newMap2(ii2,:));
+%
+newMap2 = map2 * TRANSFORM.T + ...
+    repmat(TRANSFORM.c(1,:),length(index2),1);
+newMap = map1;
+newMap(ii1,:) = 0.5 *(map1(ii1,:) + newMap2(ii2,:));
 
 % end
 
