94c1abce490ca2b653a12feb4ac9dd198c3f94fa
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-01-02 01:30:00 +0000

    select anchors by coordinates

--- cca/cca/network/selectAnchorNodesCloseToPoints.m
+++ cca/cca/network/selectAnchorNodesCloseToPoints.m
@@ -21,17 +21,21 @@ anchor=0;
 min=-1;
 fprintf(1,'Getting anchor close to: (%.1f,%.1f)\n',target(1,1),target(1,2));
 for i=1:size(points,1)
-    distance=disteusq(points(i),target,'s');
-    fprintf(1, '%.0f Point (%.2f,%.2f) %.3f\n',i,points(i,1),points(i,2),distance);
+    d=pdistance(points(i,:),target');
+    fprintf(1, '%.0f Point (%.2f,%.2f) %.3f\n',i,points(i,1),points(i,2),d);
     if points(i,1) < 1.5 && points(i,1) > 8.5
-        fprintf(1,'----Distance %.2f (%.1f,%.1f)\n',distance,points(i,1),points(i,2));
+        fprintf(1,'----Distance %.2f (%.1f,%.1f)\n',d,points(i,1),points(i,2));
     end
-    if anchor == 0 || ((points(i,1) ~= 0 && points(i,2) ~= 0) && distance < min && size(nonzeros(ismember(exclude,i)),1) < 1)
+    if anchor == 0 || ((points(i,1) ~= 0 && points(i,2) ~= 0) && d < min && size(nonzeros(ismember(exclude,i)),1) < 1)
         if (anchor ~= 0)
-            fprintf(1, 'Closer %.3f (%.2f,%.2f) than %.3f (%.2f,%.2f)\n', distance,points(i,1),points(i,2),min,points(anchor,1),points(anchor,2));
+            fprintf(1, 'Closer %.3f (%.2f,%.2f) than %.3f (%.2f,%.2f)\n', d,points(i,1),points(i,2),min,points(anchor,1),points(anchor,2));
         end
         anchor=i;
-        min=distance;
+        min=d;
     end
 end
+return;
+
+function [dist]=pdistance(p1,p2)
+dist=sqrt((p2(1)-p1(1)).^2 + (p2(2)-p1(2)).^2);
     
