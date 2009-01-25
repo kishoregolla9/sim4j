653b2373581c454cd9a335c907eef729961de304
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-01-25 17:28:08 +0000

    cleanup

--- cca/cca/simcca.m
+++ cca/cca/simcca.m
@@ -3,6 +3,7 @@ if exist('folder','var') == 0
     mkdir folder
 end
 
+%% BuildNetwork
 hold off
 addpath('cca')
 addpath('network')
@@ -10,22 +11,24 @@ addpath('network')
 tic;
 networkconstants;
 
-radius=1.6;
-step=0.4;
-numSteps=5;
+radius=1.5;
+step=0.5;
+numSteps=3;
 minRadius=1.6;
 maxRadius=1.4+(step*numSteps);
 
 shape=SHAPE_SQUARE;
 placement=NODE_GRID;
-N=120;
-networkEdge=8;
+N=40;
+networkEdge=5;
 ranging=0; % range-free
 numAnchors=3;
-numAnchorSets=5;
+numAnchorSets=1;
 
 [sourceNetwork]=buildNetwork(shape,placement,networkEdge,networkEdge,N);
 [anchors]=buildAnchors(sourceNetwork,ANCHORS_SPREAD,numAnchors,numAnchorSets);
+
+%% RUNCCA
 close(gcf);
 for i=1 : numSteps+1
 
@@ -36,27 +39,7 @@ for i=1 : numSteps+1
     if (~network.connected), return, end
     network.anchors=anchors;
     plotNetwork(network,folder);
-
-    %or anchorNodesSelectionSquare100.m or other similar functions (e.g., SingleNodeSelection.m)
-    %to get anchors or anchor sets. Sometimes, you get error when running these scripts/functions.
-    %That often means the area where you want to select an anchor node has no node in it to be selected.
-    %The SingleNodeSelection.m may be used to get anchor nodes in desired area one by one.
-    %Can also randomly pick up the node index to form the "anchor",
-    %e.g.,
-%    anchors=[2, 3, 5];
-    %has two three anchor node sets.
-    % Can have multiple anchor sets. So may need to form
-    %'anchor' which is an matrix of MxN (M sets with each set has N anchor nodes. M>=1, N>=3)
-
-    %(7)Select starting nodes for map patch. May use
-
-    % minNodeChoice=1;
-    % anchorSelectX1=1;
-    % anchorSelectY1=4;
-    % anchorSelectX2=3;
-    % anchorSelectY2=6;
-    % [startNode]=SingleNodeSelection(network,minNodeChoice,anchorSelectX1,anchorSelectX2,anchorSelectY1,anchorSelectY2);
-
+    
     %to pick nodes from different part of the network. Should form a startNode=[a b c ...] array that
     %contains the starting node for map patching that want to experiment with.
     %For example,
