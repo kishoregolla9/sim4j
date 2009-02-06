a0712a2702389a4ff3bd4a395d0a47f857cd4b59
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-02-06 03:39:59 +0000

    plot diffs

--- cca/cca/network/plotNetworkDiff.m
+++ cca/cca/network/plotNetworkDiff.m
@@ -1,17 +1,14 @@
-function [h]=plotNetworkDiff(network,mappedPoints,anchors,filename)
+function [h]=plotNetworkDiff(network,mappedPoints,anchors,plottitle,filename)
 % Plot the network, showing the anchor nodes with red circles
 
-figurename=sprintf('%s',network.shape);
-h=figure('Name',figurename);
-hold off
-clf
+h=figure('Name',plottitle);
 hold all
 for i=1:size(network.points,1)
     plot([network.points(i,1),mappedPoints(i,1)],...
         [network.points(i,2),mappedPoints(i,2)],'-dr');
 end
 plot(network.points(:,1),network.points(:,2),'db');
-title({network.shape});
+title(plottitle);
 
 for a=1:size(anchors,2)
     plot(network.points(anchors(:,a),1),network.points(anchors(:,a),2),'-s',...
