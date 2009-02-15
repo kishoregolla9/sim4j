37708272ba7084178e10a9f510eb705fee413708
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-02-15 17:34:52 +0000

    better bar plot

--- cca/cca/plot/plotAnchorSetVsError.m
+++ cca/cca/plot/plotAnchorSetVsError.m
@@ -8,31 +8,34 @@ h=figure('Name','The Results By Anchor');
 
 numAnchorSets=size(result(1).anchors,1);
 numConnectivityPoints=size(result,2);
-medians=zeros(numConnectivityPoints,numAnchorSets);
-mins=zeros(numConnectivityPoints,numAnchorSets);
-maxs=zeros(numConnectivityPoints,numAnchorSets);
+medianErrors=zeros(numConnectivityPoints,numAnchorSets);
+meanErrors=zeros(numConnectivityPoints,numAnchorSets);
+minErrors=zeros(numConnectivityPoints,numAnchorSets);
+maxErrors=zeros(numConnectivityPoints,numAnchorSets);
 for i=1:size(result,2)
     if result(i).connectivity > 10
-        medians(i,:)=result(i).medianErrorPerAnchorSet;
-        mins(i,:)=result(i).minErrorPerAnchorSet;
-        maxs(i,:)=result(i).maxErrorPerAnchorSet;
+        maxErrors(i,:)=result(i).maxErrorPerAnchorSet;
+        medianErrors(i,:)=result(i).medianErrorPerAnchorSet;
+        meanErrors(i,:)=result(i).meanErrorPerAnchorSet;
+        minErrors(i,:)=result(i).minErrorPerAnchorSet;
     else
         fprintf(1,'Result %.0f has a low connectivity (%f)\n',i,result.connectivity);
     end
 end
 
-bar3([median(maxs);median(medians);median(mins)],'detached');
+dataToPlot=[mean(maxErrors);mean(meanErrors);mean(medianErrors);mean(minErrors)];
+bar3(dataToPlot,'detached');
 grid on
-legend('Median Error','Max Error','Min Error');
 plotTitle=sprintf('Network %s',network.shape);
-title({'Median Error for Connectivities > 10',plotTitle});
-ylabel('Anchor Set');
-zlabel('Location Error (factor of radius)');
-% hold off
+title({plotTitle,'Median of Error Statistic','for radii making network with connectivity > 10'});
+zlabel({'Location Error','(factor of radius)'});
+xlabel('Anchor Set');
+ylabel('Error Statistic');
+set(gca,'YTickLabel','Max|Mean|Median|Min');
 
 filename=sprintf('%s\\AnchorSetsVsError-%s-Radius%.1f-to-%.1f.eps',...
-   folder,network.shape,minRadius,maxRadius);
+    folder,network.shape,minRadius,maxRadius);
 print('-depsc',filename);
 filename=sprintf('%s\\AnchorSetsVsError-%s-Radius%.1f-to-%.1f.png',...
-   folder,network.shape,minRadius,maxRadius);
+    folder,network.shape,minRadius,maxRadius);
 print('-dpng',filename);
