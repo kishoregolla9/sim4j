6b9741748c7c5e8e120de5c5ae5c8c3a9c03efe7
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-02-16 02:58:54 +0000

    plot errors with diffs

--- cca/cca/plot/plotResult.m
+++ cca/cca/plot/plotResult.m
@@ -2,6 +2,7 @@ function [h1]=plotResult(results,radii,folder)
 
 h1=plotConnectivityVsError(results,radii,folder);
 plotAnchorSetVsError(results,radii,folder);
+plotAnchorNeighborsVsError(results,radii,folder);
 
 
 
