3891c63e7f03bee528da0bb9317b97fb9ea9cf4d
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-02-24 02:49:36 +0000

    more plotting

--- cca/cca/plot/plotDistanceVsError.m
+++ cca/cca/plot/plotDistanceVsError.m
@@ -7,37 +7,53 @@ network=results(1).network;
 points=network.points;
 numAnchorSets=size(network.anchors,1);
 
-%for i=1:numAnchorSets
-for i=1:1
+figure('Name','Distance to Anchor vs Error');
+
+for i=1:numAnchorSets
     hold off
+    subplot(ceil(numAnchorSets/2),ceil(numAnchorSets/2),i);
+    hold all
     t=sprintf('Anchor Set %i',i);
     title(t);
-    hold all
     distances=zeros(size(points,1),1);
 
     for p=1:size(distances,1)
         numAnchors=size(network.anchors,2);
         distToAnchor=zeros(numAnchors,1);
         for a=1:numAnchors
-            distToAnchor(i)=distance(points(network.anchors(a),:),points(p,:));
+            distToAnchor(a)=distance(points(network.anchors(a),:),points(p,:));
         end
         distances(p)=sum(distToAnchor);
     end
     
+    labels=cell(1, size(results,2));
     for r=1:size(results,2)
         errors=sum(results(r).localMaps(i).differenceVector,2)/results(r).radius;
-        dataToPlot=sort([distances,errors],1);
-        plot(dataToPlot(:,1),dataToPlot(:,2));
+        dataToPlot=sortrows([distances,errors]);
+        x=dataToPlot(:,1);
+        y=dataToPlot(:,2);
+        windowSize=25;
+        yy=filter(ones(1,windowSize)/windowSize,1,y);
+        plot(x,yy);
         labels{r}=sprintf('Radius=%.1f',results(r).radius);
     end
-    legend(labels);
+    legend(labels,'Location','NorthWest');
+    xlabel('Distance to Nearest Anchor');
+    ylabel('Location Error (factor of radius)');
+
+    units=get(gcf,'units');
+    set(gcf,'units','normalized','outerposition',[0 0 1 1]);
+    set(gcf,'units',units);
+    
+    filename=sprintf('%s\\DistanceVsError-%s-Radius%.1f-to-%.1f-AnchorSet%i.eps',...
+        folder,network.shape,minRadius,maxRadius,i);
+    print('-depsc',filename);
+    filename=sprintf('%s\\DistanceVsError-%s-Radius%.1f-to-%.1f-AnchorSet%i.png',...
+        folder,network.shape,minRadius,maxRadius,i);
+    print('-dpng',filename);
 end
-filename=sprintf('%s\\DistanceVsError-%s-Radius%.1f-to-%.1f.eps',...
-    folder,network.shape,minRadius,maxRadius);
-print('-depsc',filename);
-filename=sprintf('%s\\DistanceVsError-%s-Radius%.1f-to-%.1f.png',...
-    folder,network.shape,minRadius,maxRadius);
-print('-dpng',filename);
+
+hold off
 
 end
 
