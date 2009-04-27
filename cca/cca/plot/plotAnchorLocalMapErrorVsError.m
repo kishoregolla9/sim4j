3763785eab00a8dd49a940ea148593ff842ce44a
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-04-27 02:10:55 +0000

    try to plot local map error

---
+++ cca/cca/plot/plotAnchorLocalMapErrorVsError.m
@@ -0,0 +1,50 @@
+function [ h ] = plotAnchorLocalMapErrorVsError( results,radii,folder )
+
+network=results.network;
+minRadius=radii(1);
+maxRadius=radii(size(radii,2));
+
+h=figure('Name','The Results by Connectivity');
+hold off
+
+numAnchorSets=size(network.anchors,1);
+labels=cell(size(results,2),1);
+for r=1:size(results,2)
+    hold all
+    sumLocalMapError=zeros(numAnchorSets,1);
+    for a=1:numAnchorSets
+        sumLocalMapError(a)=sumLocalMapError(a)+...
+            sum(results(r).localMaps{1}(a).local_coordinates_error_mean);
+    end
+    normalized=normalize(sumLocalMapError);
+    plot(normalized,[results(r).meanError],'-o');
+    labels{r}=sprintf('Radius %.2f', results(r).radius);
+    hold all;
+end
+
+grid on
+plotTitle=sprintf('Network %s',network.shape);
+title({'Localization Error',plotTitle});
+xlabel('Sum of Local Map Errors for all Anchors');
+ylabel('Location Error (factor of radius)');
+legend(labels);
+hold off;
+ 
+filename=sprintf('AnchorLocalMapError-vs-Error-%s-Radius%.1f-to-%.1f.eps',...
+   network.shape,minRadius,maxRadius);
+saveFigure(folder,filename);
+
+function [ndat] = normalize(dat)
+m = size(dat,2);
+ndat = dat;
+norms = zeros(m,1);
+for i = 1:m
+    theNorm=norm(ndat(:,i));
+    if theNorm ~= 0
+        norms(i) = theNorm;
+        ndat(:,i) = ndat(:,i)/norms(i);
+    else
+        fprintf(1,'The norm of sample %g is 0, sample not normalized!',i);
+    end
+end
+
