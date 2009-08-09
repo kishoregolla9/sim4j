2f4992db4d305b2dabfc092d1cd22e2dfe72009a
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-08-09 18:23:06 +0000

    load results from files and struture-ize results

--- cca/cca/cca/compareMaps.m
+++ cca/cca/cca/compareMaps.m
@@ -1,17 +1,18 @@
-function [node] = compareMaps(node, mappedResult)
+function [node] = compareMaps(network, node, mappedResult, radius)
 
 % set output
 % t.xyEstimate = mappedResult;
-node(startNode).patched_network_transform=mappedResult;
+distanceMatrix=network.distanceMatrix;
+node.patched_network_transform=mappedResult;
 D_C = sqrt(disteusq(mappedResult,mappedResult,'x'));
 
 differenceVector=abs(mappedResult-network.points);
 
 D_dist_mean = mean((mean(abs(D_C-distanceMatrix)))');
-D_dist_mean=D_dist_mean/r;
-node(startNode).patched_net_dist_error_mean=D_dist_mean;
+D_dist_mean=D_dist_mean/radius;
+node.patched_net_dist_error_mean=D_dist_mean;
 
-node(startNode).differenceVector=differenceVector;
-node(startNode).mappedPoints=mappedResult;
+node.differenceVector=differenceVector;
+node.mappedPoints=mappedResult;
 
 return;
--- cca/cca/cca/transformMap.m
+++ cca/cca/cca/transformMap.m
@@ -1,5 +1,5 @@
 function [mappedResult] = transformMap(points, refineResult, anchorNodes)
-
+N=size(points,1);
 [D, Z, TRANSFORM] = procrustes(points(anchorNodes,:), refineResult(anchorNodes,1:2));
 mappedResult = TRANSFORM.b * refineResult(:,1:2) * TRANSFORM.T + ...
     repmat(TRANSFORM.c(1,:),N,1);
