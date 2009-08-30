19ee20255e879c38aa6417b1ae5d22eb20a3c536
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-08-30 21:06:21 +0000

    make all the plotResults work

--- cca/cca/plot/plotAnchorLocalMapErrorVsError.m
+++ cca/cca/plot/plotAnchorLocalMapErrorVsError.m
@@ -4,7 +4,7 @@ network=results.network;
 minRadius=radii(1);
 maxRadius=radii(size(radii,2));
 
-h=figure('Name','Anchor Node Local Map Error vs Final Error');
+h=figure('Name','Anchor Set Local Map Error vs Final Error');
 hold off
 
 numAnchorSets=size(anchors,1);
@@ -12,17 +12,16 @@ labels=cell(size(results,2),1);
 for r=1:size(results,2)
     hold all
     sumLocalMapError=zeros(numAnchorSets,1);
+    errorPerAnchorSet=zeros(numAnchorSets,1);
     for s=1:numAnchorSets
         anchorSet=anchors(s,:);
         for a=1:size(anchorSet,2)
             sumLocalMapError(s)=sumLocalMapError(s)+...
                 sum(allMaps(r,anchorSet(a)).local_coordinates_error_mean);
         end
+        errorPerAnchorSet(s)=mean([results(r).errors(s,:).mean],2);
     end
-    normalized=normalize(sumLocalMapError);
-    means=[results(r).errors(:).mean];
-    errorPerAnchorSet=mean(means,1)';
-    dataToPlot=[sumLocalMapError normalized];
+    dataToPlot=[sumLocalMapError./3 errorPerAnchorSet];
     dataToPlot=sortrows(dataToPlot,1);
     plot(dataToPlot(:,1),dataToPlot(:,2),'-o');
     labels{r}=sprintf('Radius %.2f', results(r).radius);
@@ -32,7 +31,7 @@ end
 grid on
 plotTitle=sprintf('Network %s',network.shape);
 title({'Anchor Node Local Map Error vs Final Location Error',plotTitle});
-xlabel('Sum of Local Map Errors for each Anchor Set');
+xlabel('Mean of Local Map Errors for each Anchor Set');
 ylabel('Location Error (factor of radius)');
 legend(labels);
 hold off;
--- cca/cca/simcca.m
+++ cca/cca/simcca.m
@@ -9,8 +9,8 @@ networkconstants;
 
 minRadius=2.5;
 step=1;
-numSteps=2;
-maxRadius=minRadius+(step*numSteps);
+numSteps=3;
+maxRadius=minRadius+(step*(numSteps+1));
 
 radii=minRadius:step:maxRadius;
 
@@ -18,14 +18,13 @@ shape=NET.SHAPE_SQUARE;
 placement=NET.NODE_RANDOM;
 numNodes=16;
 networkEdge=4;
-MOD_RANDOM_ANCHORS=1;
 % numNodes=36;
 % networkEdge=6;
 % MOD_RANDOM_ANCHORS=50;
 
 ranging=0; % range-free
 numAnchorsPerSet=3;
-numAnchorSets=2;
+numAnchorSets=500;
 numStartNodes=16;
 
 shapeLabel=buildNetworkShape(shape,placement,networkEdge,networkEdge,numNodes);
@@ -64,7 +63,7 @@ else
 end
 
 %% Build Local Maps
-for i=1 : numSteps+1
+for i=1 : numSteps
     localMapsFilename=sprintf('%s/localMaps/localMaps-%i.mat',folder,i);
     if (exist(localMapsFilename,'file') == 0)
         network=networks(i);
@@ -87,7 +86,7 @@ if (exist(filename,'file') ~= 0)
     fprintf(1,'Loading anchors from %s\n',filename);
     load(filename);
 else
-    [anchors]=buildAnchors(sourceNetwork,NET.ANCHORS_RANDOM,numAnchorsPerSet,numAnchorSets,MOD_RANDOM_ANCHORS);
+    [anchors]=buildAnchors(sourceNetwork,NET.ANCHORS_RANDOM,numAnchorsPerSet,numAnchorSets);
 %     [spreadAnchors]=buildAnchors(sourceNetwork,NET.ANCHORS_SPREAD,3,5);
 %     anchors=[randomAnchors;spreadAnchors];
     save(filename, 'anchors','numAnchorSets');
@@ -102,10 +101,10 @@ startNodeIncrement=numNodes/numStartNodes;
 startNodes=1:startNodeIncrement:size(networks(1).points,1);
 
 %% MAP PATCHING
-localMapsFilename=sprintf('%s/localMaps/localMaps-%i.mat',folder,numSteps+1);
+localMapsFilename=sprintf('%s/localMaps/localMaps-%i.mat',folder,numSteps);
 load(localMapsFilename);
-allMaps(numSteps+1,:)=localMaps;
-for i=1 : numSteps+1
+allMaps(numSteps,:)=localMaps;
+for i=1 : numSteps
     localMapsFilename=sprintf('%s/localMaps/localMaps-%i.mat',folder,i);
     load(localMapsFilename);
     network=networks(i);
@@ -114,26 +113,25 @@ for i=1 : numSteps+1
     resultFilename=sprintf('%s/result-%i.mat',folder,i);
     if (exist(resultFilename,'file') ~= 0)
         fprintf(1,'Loading results from %s\n',resultFilename);
-        load(filename);
+        load(resultFilename);
     else
         disp('------------------------------------')
-        patchNumber=sprintf('Map patch #%i of %i for Radius %.1f',i,numSteps+1,radius);
+        patchNumber=sprintf('Map patch #%i of %i for Radius %.1f',i,numSteps,radius);
         fprintf('Doing %s\n',patchNumber);
         result=mapPatch(network,localMaps,startNodes,anchors,network.radius,patchNumber,folder);
         fprintf(1,'Done in %f sec for %s\n',result.mapPatchTime,patchNumber);
         save(resultFilename,'result');
-
 %         plotNetworkDiff(result,anchors,folder);
     end
     if ~exist('results','var')
         % preallocate
-        results(size(numSteps+1,1))=result; %#ok<AGROW>
+        results(size(numSteps,1))=result; %#ok<AGROW>
     end
     results(i)=result; %#ok<AGROW>
 end
 
 %% PLOT RESULT
-%plotResult(results,anchors,radii,folder,allMaps);
+plotResult(results,anchors,radii,folder,allMaps);
 totalTime=toc;
 fprintf(1,'Done %i radius steps in %.3f min (%.3f sec/step) (%.3f sec/node)\n',...
     numSteps,totalTime/60,totalTime/numSteps,totalTime/(numSteps*numNodes))
@@ -143,10 +141,10 @@ for s=1:size(anchors,1)
     for r=1:size(results,2);
         radius=results(r).radius;
         network=networks(r);
+        suffix=sprintf('AnchorSet%i',s);
         filename=sprintf('networks/radius%.1f/network-%s-Radius%.1f-%s',radius,network.shape,radius,suffix);
         if (exist(filename,'file') == 0)
             fprintf('Plotting anchor set %i of %i for radius %.1f\n',s,size(anchors,1),radius);
-            suffix=sprintf('AnchorSet%i',s);
             h=plotNetwork(network,anchors(s,:),folder,suffix,results(r),s);
             saveFigure(folder,filename,h);
             close
