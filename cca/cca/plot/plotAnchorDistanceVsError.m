bc4c342abd0f90e0c0c47a67b52b405928876e80
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-09-01 02:26:38 +0000

    plot number of anchor neighbors

--- cca/cca/plot/plotAnchorDistanceVsError.m
+++ cca/cca/plot/plotAnchorDistanceVsError.m
@@ -7,6 +7,7 @@ maxRadius=radii(size(radii,2));
 network=results(1).network;
 numAnchorSets=size(anchors,1);
 
+%% Plot by Perimeter
 figure('Name','Anchor Distance vs Error');
 plotTitle=sprintf('Network %s',network.shape);
 title({'Sum of Distance between All Anchors vs Localization Error',...
@@ -23,6 +24,7 @@ for s=1:numAnchorSets
     d(3)=network.distanceMatrix(anchorNodes(3),anchorNodes(1));
     areas(s,1)=areaOfTriangle(d(1),d(2),d(3));
 	distances(s,1)=sum(d);
+    
 end
 
 hold all
@@ -49,6 +51,7 @@ filename=sprintf('AnchorDistanceVsError-%s-Radius%.1f-to-%.1f',...
     network.shape,minRadius,maxRadius);
 saveFigure(folder,filename);
 
+%% Plot By Area
 figure('Name','Anchor Triange Area vs Error');
 plotTitle=sprintf('Network %s',network.shape);
 title({'Area of Triangle made by Anchors vs Localization Error',...
@@ -77,8 +80,49 @@ filename=sprintf('AnchorTriangleAreaVsError-%s-Radius%.1f-to-%.1f',...
     network.shape,minRadius,maxRadius);
 saveFigure(folder,filename);
 
+
+%% Plot By Number Covered Nodes
+figure('Name','Num Anchor Neighbors vs Error');
+plotTitle=sprintf('Network %s',network.shape);
+title({'Number of Anchor Neighbors vs Localization Error',...
+    plotTitle});
+hold all
+grid on
+labels=cell(1, size(results,2));
+for r=1:size(results,2)
+
+    numNeighbors=zeros(numAnchorSets,1);
+    for s=1:numAnchorSets
+        % For one start node
+        errorPerAnchorSet(s)=[results(r).errors(s,1).median];
+        anchorNodes=anchors(s,:);
+        n=getNumUnique(network,anchorNodes);
+        numNeighbors(s,1)=numNeighbors(s,1) + n;
+    end    
+    
+    dataToPlot=[numNeighbors, errorPerAnchorSet];
+    dataToPlot=sortrows(dataToPlot,1);    
+    plot(dataToPlot(:,1),dataToPlot(:,2),'-o');
+    labels{r}=sprintf('Radius=%.1f',results(r).radius);
+end
+legend(labels,'Location','NorthEast');
+xlabel('Area of Triangle Anchors');
+ylabel('Median Location Error');
+hold off
+
+filename=sprintf('AnchorNeighborsVsError-%s-Radius%.1f-to-%.1f',...
+    network.shape,minRadius,maxRadius);
+saveFigure(folder,filename);
+
 end
 
+function [num] = getNumUnique(network,anchorSet)
+    n=[];
+    for i=1:size(anchorSet,2)
+        n=[n network.nodes(anchorSet(1,i)).neighbors];
+    end
+    num=size(unique(n),2);
+end
 
 function [triangleArea] = areaOfTriangle(a,b,c)
 s=(a+b+c)/2;
