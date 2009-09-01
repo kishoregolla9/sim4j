2b92d0263b02da6893548af49094d7949c6b0ce4
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-09-01 04:20:38 +0000

    plot anchor triangle stats

--- cca/cca/plot/plotAnchorAreaVsError.m
+++ cca/cca/plot/plotAnchorAreaVsError.m
@@ -1,4 +1,4 @@
-function plotAnchorDistanceVsError( results,anchors,radii,folder )
+function plotAnchorAreaVsError( results,anchors,radii,folder )
 % Plot distance between the anchors themselves vs error
 
 minRadius=radii(1);
@@ -7,13 +7,8 @@ maxRadius=radii(size(radii,2));
 network=results(1).network;
 numAnchorSets=size(anchors,1);
 
-%% Plot by Perimeter
-figure('Name','Anchor Distance vs Error');
-plotTitle=sprintf('Network %s',network.shape);
-title({'Sum of Distance between All Anchors vs Localization Error',...
-    plotTitle});
-
-distances=zeros(numAnchorSets,1);
+%% Plot by Area
+areas=zeros(numAnchorSets,1);
 for s=1:numAnchorSets
     anchorNodes=anchors(s,:);
     numAnchors=size(anchorNodes,2);
@@ -21,9 +16,14 @@ for s=1:numAnchorSets
     d(1)=network.distanceMatrix(anchorNodes(1),anchorNodes(2));
     d(2)=network.distanceMatrix(anchorNodes(2),anchorNodes(3));
     d(3)=network.distanceMatrix(anchorNodes(3),anchorNodes(1));
-    distances(s,1)=sum(d);
+    areas(s,1)=heron(d(1),d(2),d(3));
 end
 
+%% Plot By Area
+figure('Name','Anchor Triangle Area vs Error');
+plotTitle=sprintf('Network %s',network.shape);
+title({'Area of Triangle made by Anchors vs Localization Error',...
+    plotTitle});
 hold all
 grid on
 labels=cell(1, size(results,2));
@@ -31,21 +31,27 @@ for r=1:size(results,2)
     errorPerAnchorSet=zeros(numAnchorSets,1);
     for s=1:numAnchorSets
         % For one start node
-        errorPerAnchorSet(s)=[results(r).errors(s,1).max];
+        errorPerAnchorSet(s)=[results(r).errors(s,1).median];
     end    
     
-    dataToPlot=[distances, errorPerAnchorSet];
+    dataToPlot=[areas, errorPerAnchorSet];
     dataToPlot=sortrows(dataToPlot,1);    
     plot(dataToPlot(:,1),dataToPlot(:,2),'-o');
     labels{r}=sprintf('Radius=%.1f',results(r).radius);
 end
 legend(labels,'Location','NorthEast');
-xlabel('Sum of Distance between Anchors');
+xlabel('Area of Triangle Anchors');
 ylabel('Median Location Error');
 hold off
 
-filename=sprintf('AnchorDistanceVsError-%s-Radius%.1f-to-%.1f',...
+filename=sprintf('AnchorTriangleAreaVsError-%s-Radius%.1f-to-%.1f',...
     network.shape,minRadius,maxRadius);
 saveFigure(folder,filename);
 
+end
+
+%% Heron's Area of a Triangle formula
+function [triangleArea] = heron(a,b,c)
+s=(a+b+c)/2;
+triangleArea=sqrt(s*(s-a)*(s-b)*(s-c));
 end
