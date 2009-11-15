7ff02c91b674d7af833e2214a64fb96526242d03
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2009-11-15 14:35:41 +0000

    make computational branch

---
+++ network/plotNetworkDiffs.m
@@ -0,0 +1,129 @@
+function []=plotNetworkDiffs(result,allAnchors,folder,prefix)
+% Plot the network difference, showing the anchor nodes with green squares
+% Blue points are real point locations
+% Red points are mapped point locations
+
+NUM_MAX_TO_SHOW=3;
+
+r=result.radius;
+network=result.network;
+plottitle=sprintf('%s Radius %.1f',network.shape,r);
+
+numAnchorSets=size(allAnchors,1);
+patchedMaps=result.patchedMap;
+realPoints=network.points;
+shape=network.shape;
+errors=result.errors;
+% Plot a network diff diagram for each anchor set
+for j=1:numAnchorSets
+    startPlotting=tic;
+    h=figure('Name',['Network Difference' plottitle],'visible','off');
+    fprintf('Plotting Network Difference for Anchor Set #%i %s',j,prefix);
+    mappedPoints=patchedMaps(j).mappedPoints;
+    anchors=allAnchors(j,:);
+    
+    subplotTitle=sprintf('Anchor Set %i',j);
+
+    stats=sprintf('Max: %.2f Mean: %.2f Min: %.2f',...
+        mean([errors(j,:).max]),...
+        mean([errors(j,:).mean]),...
+        mean([errors(j,:).min]));
+    
+    title({subplotTitle,prefix,stats});
+    hold all
+    % Show a line from each real to each mapped point (red circles)
+    for i=1:size(realPoints,1)
+        plot([realPoints(i,1),mappedPoints(i,1)],...
+            [realPoints(i,2),mappedPoints(i,2)],'-or','MarkerSize',3);
+    end
+    % Overlay the real points with blue diamonds
+    % to distinguish them from the mapped points
+    plot(realPoints(:,1),realPoints(:,2),'db','MarkerSize',3);
+    % Overlay the mapped points with cyan circles
+    % to distinguish them from the mapped points
+    plot(mappedPoints(:,1),mappedPoints(:,2),'oc','MarkerSize',3);
+    
+    % Show the worst (max error) points with stars
+    differenceVector=patchedMaps(j).differenceVector;
+    m=getMaxErrorPoints(differenceVector,NUM_MAX_TO_SHOW);
+    for i=1:size(m,1)
+        %p=pentagram(star),k=black
+        plot(realPoints(m(i),1),realPoints(m(i),2),'pk','MarkerSize',12);
+    end
+    m=getMinErrorPoints(differenceVector,NUM_MAX_TO_SHOW);
+    for i=1:size(m,1)
+        %p=pentagram(star),g=green
+        plot(realPoints(m(i),1),realPoints(m(i),2),'pg','MarkerSize',12);
+    end
+    
+    % Show a circle of the radius around each anchor point
+    % and Draw the Anchor Triangle
+    for a=1:size(anchors,2)
+        xa=realPoints(anchors(:,a),1);
+        ya=realPoints(anchors(:,a),2);
+        plot(xa,ya,'-d',...
+            'MarkerEdgeColor','k',...
+            'MarkerFaceColor','g',...
+            'MarkerSize',5);
+        rectangle('Position',[xa-r,ya-r,r*2,r*2],'Curvature',[1,1]);
+        
+        % A line of the triangle
+        b=mod(a,size(anchors,2))+1;
+        xb=realPoints(anchors(:,b),1);
+        yb=realPoints(anchors(:,b),2);
+        line([xa,xb],[ya,yb],'LineWidth',1,'Color','green');
+    end
+
+    % Draw a rectangle around the "real" area
+    width=ceil(max(realPoints(:,1)));
+    height=ceil(max(realPoints(:,2)));
+    rectangle('Position',[0,0,width,height],'Curvature',[0,0],...
+        'LineWidth',2,'LineStyle','--');
+
+    minX=floor(min(min(realPoints(:,1),min(mappedPoints(:,1)))));
+    minY=floor(min(min(realPoints(:,1),min(mappedPoints(:,1)))));
+    maxX=ceil(max(max(realPoints(:,1),max(mappedPoints(:,1)))));
+    maxY=ceil(max(max(realPoints(:,2),max(mappedPoints(:,2)))));
+    minAll=min(minX,minY);
+    maxAll=max(maxX,maxY);
+    axis([minAll maxAll minAll maxAll]);
+    grid on
+    hold off    
+    
+    fprintf(' - done in %.2f seconds\n',minX,minY,maxX,maxY,toc(startPlotting));
+    filename=sprintf('networkdiffs/NetworkDifference-%s-Radius%.1f-AnchorSet%i-%s',shape,r,j,prefix);
+    saveFigure(folder,filename,h);
+    hold off
+    close
+end
+
+end
+
+function [m]=getMaxErrorPoints(differenceVector,num)
+m=zeros(num,1);
+errors=zeros(num,1);
+for i=1:size(differenceVector,1)
+    thisError=sum(differenceVector(i,:));
+    [c,minIndex]=min(errors);
+    if thisError > c
+        m(minIndex)=i;
+        errors(minIndex)=thisError;
+    end
+end
+end
+
+function [m]=getMinErrorPoints(differenceVector,num)
+m=ones(num,1);
+errors=ones(num,1);
+errors=errors.*1000000;
+for i=1:size(differenceVector,1)
+    thisError=sum(differenceVector(i,:));
+    [c,maxIndex]=max(errors);
+    if thisError < c
+        m(maxIndex)=i;
+        errors(maxIndex)=thisError;
+    end
+end
+
+end
+
---
+++ plot/plotAnchorAreaVsError.m
@@ -0,0 +1,83 @@
+function plotAnchorAreaVsError( results,anchors,folder )
+% Plot distance between the anchors themselves vs error
+
+minRadius=results(1).radius;
+maxRadius=results(end).radius;
+
+network=results(1).network;
+numAnchorSets=size(anchors,1);
+
+%% Plot by Area
+areas=zeros(numAnchorSets,1);
+distances=zeros(numAnchorSets,1);
+heights=zeros(numAnchorSets,3);
+for s=1:numAnchorSets
+    anchorNodes=anchors(s,:);
+    numAnchors=size(anchorNodes,2);
+    d=zeros(numAnchors*2-1,1);
+    d(1)=network.distanceMatrix(anchorNodes(1),anchorNodes(2));
+    d(2)=network.distanceMatrix(anchorNodes(2),anchorNodes(3));
+    d(3)=network.distanceMatrix(anchorNodes(3),anchorNodes(1));
+    areas(s,1)=heron(d(1),d(2),d(3));
+    c=centroid(network.points(anchorNodes(:),:));
+    distances(s,1)=distance([network.height/2 network.width/2],c);
+    
+    A=areas(s,1);
+    h=zeros(3,1);
+    h(1)=2*A/d(2);
+    h(2)=2*A/d(3);
+    h(3)=2*A/d(1);
+    heights(s,1)=max(h);
+    heights(s,2)=median(h);
+    heights(s,3)=min(h);
+end
+
+hold off
+fig=figure('Name','Anchor Triangle Area vs Error','visible','off');
+plotTitle=sprintf('Network %s',network.shape);
+title({'Area of Triangle made by Anchors vs Localization Error',...
+    plotTitle});
+grid on
+labels=cell(1, size(results,2));
+for r=1:size(results,2)
+    errorPerAnchorSet=zeros(numAnchorSets,1);
+    for s=1:numAnchorSets
+        % For one start node
+        errorPerAnchorSet(s)=[results(r).errors(s,1).median];
+    end    
+    
+    dataToPlot=[areas, distances, errorPerAnchorSet];
+    dataToPlot=sortrows(dataToPlot,1);    
+    scatter(dataToPlot(:,1),dataToPlot(:,3),... % X,Y of circles
+         dataToPlot(:,2).*10,...        % size of circles
+         dataToPlot(:,2));              % color of circles
+    grid on
+    labels{r}=sprintf('Radius=%.1f',results(r).radius);
+end
+legend(labels,'Location','Best');
+xlabel({'Area of Triangle Anchors',...
+    'Color/Size corresponds to distance of centroid to centre of networks'});
+ylabel('Median Location Error');
+axes1=get(fig,'CurrentAxes');
+set(axes1,'XScale','log');
+hold off
+
+filename=sprintf('AnchorTriangleAreaVsError-%s-Radius%.1f-to-%.1f',...
+    network.shape,minRadius,maxRadius);
+saveFigure(folder,filename);
+
+end
+
+%% Heron's Area of a Triangle formula
+function [triangleArea] = heron(a,b,c)
+s=(a+b+c)/2;
+triangleArea=sqrt(s*(s-a)*(s-b)*(s-c));
+end
+
+function [c] = centroid(v)
+c=[(v(1,1)+v(2,1)+v(3,1)) / 3 (v(1,2)+v(2,2)+v(3,2)) / 3];
+end
+
+function [d] = distance(a,b)
+d=sqrt((b(1)-a(1))^2 + (b(2)-a(2))^2);
+end
