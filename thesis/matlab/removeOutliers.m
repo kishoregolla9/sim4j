d1196193122d570fdf89b38bc4d84ddc3417e1c1
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-03-14 21:37:28 +0000

    helper functions

---
+++ thesis/matlab/removeOutliers.m
@@ -0,0 +1,20 @@
+function [ y ] = removeOutliers( x )
+
+mu = mean(x);
+sigma = std(x);
+[n,p] = size(x);
+% Create a matrix of mean values by
+% replicating the mu vector for n rows
+MeanMat = repmat(mu,n,1);
+% Create a matrix of standard deviation values by
+% replicating the sigma vector for n rows
+SigmaMat = repmat(sigma,n,1);
+% Create a matrix of zeros and ones, where ones indicate
+% the location of outliers
+outliers = abs(x - MeanMat) > 3*SigmaMat;
+
+y=x;
+y(find(outliers))=[]; % remove the outliers from x
+
+
+end
