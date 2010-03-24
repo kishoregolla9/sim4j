dfc5b6ed1eeb77b51a308e2be21e7a5b4c59e9c0
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-03-24 01:48:18 +0000

    probability of mean error < 0.2

--- thesis/matlab/plot/plotHistograms.m
+++ thesis/matlab/plot/plotHistograms.m
@@ -11,16 +11,26 @@ for r=1:size(results,2)
     
     hold all;
     grid on;
-    x=removeOutliers([result.errors.max]);
+    [x,outliers]=removeOutliers([result.errors.max]);
     [n,xout] = hist(x,sqrt(length(x)));
     bar(xout,n,'b','LineWidth',1);
-    x=removeOutliers([result.errors.mean]);
+    
+    x=[result.errors.mean];
+    x(outliers)=[];
     [n,xout] = hist(x,sqrt(length(x)));
+    
+    threshold=0.2;
+    pLess=sum(n(xout<threshold))/sum(n);
+    
     bar(xout,n,'c','LineWidth',1);
-    x=removeOutliers([result.errors.min]);
+    x=[result.errors.min];
+    x(outliers)=[];
     [n,xout] = hist(x,sqrt(length(x)));
     bar(xout,n,'r','LineStyle','none');    
     legend({'Max Error','Mean Error','Min Error'});
+    t=sprintf('Probability of Mean Error less than %0.2f is %0.3f%c',...
+        threshold,pLess*100,'%');
+    title(t);
     hold off;
     
     filename=sprintf('Histogram-%s-Radius%.1f',...
