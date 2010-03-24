32950f470b6a2869d4ea7f596f16ef88805d6877
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-03-24 01:48:18 +0000

    probability of mean error < 0.2

--- trunk/thesis/matlab/plot/plotAllAnchorSetErrors.m
+++ trunk/thesis/matlab/plot/plotAllAnchorSetErrors.m
@@ -53,7 +53,7 @@ for r=1:size(results,2)
         data(a,9)=deviationOfSlopes(mappedTriangle);
         
         data(a,6)=2*(det(T)<0); % if det(T)<0, then reflected
-%         data(a,10)=4*(tr.c(1,1)<0 | tr.c(1,2)<0);
+        %         data(a,10)=4*(tr.c(1,1)<0 | tr.c(1,2)<0);
         data(a,11)=3*(tr.b<0);
         
         data(a,12)=results(r).dissimilarity(a);
@@ -63,8 +63,8 @@ for r=1:size(results,2)
         %     heightOverBase=realHeight(:)./max(realEdges,[],2);
         %     mapped=mappedHeight(:)./max(mappedEdges,[],2);
         
-%         data(a,14)=realHeight(a);
-
+        %         data(a,14)=realHeight(a);
+        
         data(a,15)=results(r).ixy.errorsPerAnchorSet(a).max;
         data(a,16)=results(r).ixy.errorsPerAnchorSet(a).mean;
         data(a,17)=results(r).ixy.errorsPerAnchorSet(a).min;
@@ -76,73 +76,78 @@ for r=1:size(results,2)
     
     ax1 = gca;
     set(ax1,'XScale','log');
-    legends=cell(4,1);
-    p=plot([data(:,2),data(:,3),data(:,7)],'-o');
-    
+    legends=cell(3,1);
+    %     p=plot([data(:,2),data(:,3),data(:,7)],'-o');
+    p=plot([data(:,2),data(:,3),data(:,4)],'-o');
+    set(p(1),'Marker','^');
+    set(p(3),'Marker','v');
     plots(1)=p(1);
     legends{1}=sprintf('Max (Euclidean)');
     
     plots(2)=p(2);
     legends{2}=sprintf('Mean (Euclidean)');
-   
-    plots(3)=p(3);    
-    set(plots(3),'LineStyle','-.','Marker','v');
-    legends{3}=sprintf('Anchor Error');
-
-    X=1:size(data,1);
-    [plots(4),ax]=addaxis(X,data(:,14),':^c'); % Triangle Area
-%     set(ax,'YLim',[0 0.2]);
-%     set(ax,'YTick',0:0.05:0.2);
-    legends{4}=sprintf('Triangle area');
-    addaxislabel(2,'Triangle area ');
     
-%     plots(5)=addaxis(X,data(:,12),'-pm');
-%     legends{5}=sprintf('Dissimiliarity');
-%     addaxislabel(3,'Dissimiliarity');
+    plots(3)=p(3);
+    legends{3}=sprintf('Min (Euclidean)');
     
-   
-%     plots(6)=addaxis(X,data(:,18),'-s');
-%     legends{6}=sprintf('Jackknife');
-%     addaxislabel(4,'Jackknife');
+%     plots(3)=p(3);
+%     set(plots(3),'LineStyle','-.','Marker','v');
+%     legends{3}=sprintf('Anchor Error');
     
-%     plots(7)=addaxis(X,data(:,7),'-.vr'); % anchor node errors
-%     legends{7}=sprintf('Anchor Node Error');
-%     addaxislabel(4,'Anchor Node Error');
+%     X=1:size(data,1);
+%     [plots(4),ax]=addaxis(X,data(:,14),':^c'); % Triangle Area
+%     %     set(ax,'YLim',[0 0.2]);
+%     %     set(ax,'YTick',0:0.05:0.2);
+%     legends{4}=sprintf('Triangle area');
+%     addaxislabel(2,'Triangle area ');
     
-%     plots(7)=addaxis(X,data(:,9),':sr'); % Slopes
-%     legends{7}=sprintf('Std of Slopes');
-%     addaxislabel(4,'Std of Slopes');
+    %     plots(5)=addaxis(X,data(:,12),'-pm');
+    %     legends{5}=sprintf('Dissimiliarity');
+    %     addaxislabel(3,'Dissimiliarity');
     
-%     [plots(7),ax]=addaxis(X,data(:,6),[1,5],'*r','MarkerSize',10);
-%     legends{7}=sprintf('Transformation Properties');
-%     addaxislabel(4,'Transformation Properties');
-%     set(ax,'YTick',1:1:5)
-%     set(ax,'YTickLabel',{'','det(T)<1','tr.c<1','tr.b<1'})
-
-%     [plots(7),ax]=addaxis(X,data(:,14),'--^r','MarkerSize',8);
-%     legends{7}=sprintf('Std of edges');
-%     addaxislabel(4,'Std of edges');
-%     set(ax,'XGrid','on')
     
-%      plots(7)=addaxis(X,mappedHeight,'^-g','MarkerSize',8);
-%      legends{7}=sprintf('height');
-%      addaxislabel(4,'height');
-   
-%     plots(9)=addaxis(X,max(data(:,15))./max(data(:,16)),':vb'); % Triangle Edges
-%     legends{9}=sprintf('Real:Mapped triangle max edge');
-%     addaxislabel(6,'Triangle Edge Ratio');
-%     plotVsError(results,r,'Min Triangle Edge - mapped',min(realEdges,2),folder);
+    %     plots(6)=addaxis(X,data(:,18),'-s');
+    %     legends{6}=sprintf('Jackknife');
+    %     addaxislabel(4,'Jackknife');
     
-%     plots(9)=plot(X,data(:,10),'sc','MarkerSize',10); % is tr.c negative?
-%     legends{9}=sprintf('Is translation negative');
+    %     plots(7)=addaxis(X,data(:,7),'-.vr'); % anchor node errors
+    %     legends{7}=sprintf('Anchor Node Error');
+    %     addaxislabel(4,'Anchor Node Error');
     
-%     plots(10)=plot(X,data(:,11),'sg','MarkerSize',10); % is tr.b negative?
-%     legends{10}=sprintf('Is scalar negative');
-
-%     plots(11)=addaxis(X,data(:,12),':sr'); 
-%     legends{11}=sprintf('Procrustes Dissimilarity Measure');
-%     addaxislabel(6,'Procrustes Dissimilarity Measure');
-   
+    %     plots(7)=addaxis(X,data(:,9),':sr'); % Slopes
+    %     legends{7}=sprintf('Std of Slopes');
+    %     addaxislabel(4,'Std of Slopes');
+    
+    %     [plots(7),ax]=addaxis(X,data(:,6),[1,5],'*r','MarkerSize',10);
+    %     legends{7}=sprintf('Transformation Properties');
+    %     addaxislabel(4,'Transformation Properties');
+    %     set(ax,'YTick',1:1:5)
+    %     set(ax,'YTickLabel',{'','det(T)<1','tr.c<1','tr.b<1'})
+    
+    %     [plots(7),ax]=addaxis(X,data(:,14),'--^r','MarkerSize',8);
+    %     legends{7}=sprintf('Std of edges');
+    %     addaxislabel(4,'Std of edges');
+    %     set(ax,'XGrid','on')
+    
+    %      plots(7)=addaxis(X,mappedHeight,'^-g','MarkerSize',8);
+    %      legends{7}=sprintf('height');
+    %      addaxislabel(4,'height');
+    
+    %     plots(9)=addaxis(X,max(data(:,15))./max(data(:,16)),':vb'); % Triangle Edges
+    %     legends{9}=sprintf('Real:Mapped triangle max edge');
+    %     addaxislabel(6,'Triangle Edge Ratio');
+    %     plotVsError(results,r,'Min Triangle Edge - mapped',min(realEdges,2),folder);
+    
+    %     plots(9)=plot(X,data(:,10),'sc','MarkerSize',10); % is tr.c negative?
+    %     legends{9}=sprintf('Is translation negative');
+    
+    %     plots(10)=plot(X,data(:,11),'sg','MarkerSize',10); % is tr.b negative?
+    %     legends{10}=sprintf('Is scalar negative');
+    
+    %     plots(11)=addaxis(X,data(:,12),':sr');
+    %     legends{11}=sprintf('Procrustes Dissimilarity Measure');
+    %     addaxislabel(6,'Procrustes Dissimilarity Measure');
+    
     n=10;
     nWorst=sprintf('%i ',data(1:n,1));
     nBest=sprintf('%i ',data(end:-1:size(data,1)-n-1,1));
@@ -160,7 +165,7 @@ for r=1:size(results,2)
     saveFigure(folder,filename,h);
     
     hold off;
-%     close(h);
+    %     close(h);
 end
 
 end
