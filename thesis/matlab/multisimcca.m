28a5ed900991dfd673934aa56e5899abde7df78a
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-04-29 00:44:19 +0000

    extract

--- thesis/matlab/multisimcca.m
+++ thesis/matlab/multisimcca.m
@@ -111,28 +111,8 @@ for anchorSet=1:numAnchorSets
     end
 end
 clear result anchors;
-%% Plot Same-Anchors Data
-h=figure('Name','Different Networks around Same Anchors','visible','off');
-% plot(maxErrors,'-^b');
-hold all
-% plot(meanErrors,'-og');
-
-confidence=0.05;
-
-[ci]=getConfidenceInterval(confidence,maxErrors');
-mu=mean(maxErrors,2);
-errorbar(mu,ci);
-
-[ci]=getConfidenceInterval(confidence,meanErrors');
-mu=mean(meanErrors,2);
-errorbar(mu,ci);
-
-xlabel('Network Index');
-ylabel('Location Error');
-legend({'Max','Mean'});
-saveFigure(folderAll,'SameAnchors',h);
-hold off
-close
+ %%
+plotSameAnchors;
 
 %% Histogram of Moving Anchors
 h=figure('Name','Histogram','visible','off');
---
+++ thesis/matlab/plotSameAnchors.m
@@ -0,0 +1,22 @@
+%% Plot Same-Anchors Data
+h=figure('Name','Different Networks around Same Anchors','visible','off');
+% plot(maxErrors,'-^b');
+hold all
+% plot(meanErrors,'-og');
+
+confidence=0.05;
+
+[ci]=getConfidenceInterval(confidence,maxErrors');
+mu=mean(maxErrors,2);
+errorbar(mu,ci);
+
+[ci]=getConfidenceInterval(confidence,meanErrors');
+mu=mean(meanErrors,2);
+errorbar(mu,ci);
+
+xlabel('Network Index');
+ylabel('Location Error');
+legend({'Max','Mean'});
+saveFigure(folderAll,'SameAnchors',h);
+hold off
+close
