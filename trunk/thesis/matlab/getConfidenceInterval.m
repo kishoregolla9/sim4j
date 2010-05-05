e0c9dc59cef3d12799eb73e68365dea823e4d4cd
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-05-05 00:36:27 +0000

    plot with all data points too

--- trunk/thesis/matlab/getConfidenceInterval.m
+++ trunk/thesis/matlab/getConfidenceInterval.m
@@ -1,7 +1,7 @@
 function [ ci ] = getConfidenceInterval( confidence, data )
 
 n=length(data);
-criticalT=1.984;
+criticalT=tinv(confidence,size(data,2));
 % data=removeOutliers(sort(data),2);
 ci=(criticalT*std(data)/sqrt(n));
 end
--- trunk/thesis/matlab/plotSameAnchors.m
+++ trunk/thesis/matlab/plotSameAnchors.m
@@ -1,23 +1,28 @@
 %% Plot Same-Anchors Data
-h=figure('Name','Different Networks around Same Anchors','visible','off');
+h=figure('Name','Different Networks around Same Anchors');%,'visible','off');
 % plot(maxErrors,'-^b');
 hold all
 % plot(meanErrors,'-og');
 
 confidence=0.05;
 
-[ci]=getConfidenceInterval(confidence,maxErrors');
-mu=mean(maxErrors,2);
-errorbar(mu,ci,'x');
+% [ci]=getConfidenceInterval(confidence,maxErrors');
+% mu=mean(maxErrors,2);
+% errorbar(mu,ci,'x');
 
 [ci]=getConfidenceInterval(confidence,meanErrors');
-mu=mean(meanErrors,2');
+mu=mean(meanErrors,2);
 errorbar(mu,ci,'o');
 
-xlabel('Network Index');
+for i=1:size(meanErrors,1)
+   plot(repmat(i,size(meanErrors,2)),meanErrors(i,:),'rx')'
+end
+
+xlabel('Anchor Index');
 ylabel('Location Error');
-axis( [0 100 0 1] )
-legend({'Max','Mean'});
+numAnchorSets
+axis( [0 size(maxErrors,1) 0 1] )
+legend({'Mean'});
 saveFigure(folderAll,'SameAnchors',h);
 hold off
-close
+% close
