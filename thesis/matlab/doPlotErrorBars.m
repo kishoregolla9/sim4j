f2d9897f5566be7723ebc25da7adc45152682118
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-05-31 02:45:08 +0000

    plot same anchors

--- thesis/matlab/doPlotErrorBars.m
+++ thesis/matlab/doPlotErrorBars.m
@@ -1,11 +1,19 @@
-function [h] = doPlotErrorBars(data,confidence,pointSpec)
+function [h] = doPlotErrorBars(data,confidence,pointSpec,x)
 
 % plot the actual data points (under the error bars)
 for i=1:size(data,1)
-   plot(repmat(i,size(data,2)),data(i,:),pointSpec);
+    if (exist('x','var'))
+        plot(repmat(x,size(data,2)),data(i,:),pointSpec);
+    else
+        plot(repmat(i,size(data,2)),data(i,:),pointSpec);
+    end
 end
 
 [ci]=getConfidenceInterval(confidence,data');
 mu=mean(data,2);
-h=errorbar(mu,ci,'o');
+if (exist('x','var'))
+    h=errorbar(x,mu,ci,'o');
+else
+    h=errorbar(mu,ci,'o');
+end
 end
