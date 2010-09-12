b2c2a72d178ca1083d955c6de78b3461b6685889
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-09-12 18:19:40 +0000

    more related works (chapter3)

--- trunk/thesis/thesis/build.bat
+++ trunk/thesis/thesis/build.bat
@@ -1,5 +1,6 @@
-del *.aux *.dvi *.lof *.ps
+del *.aux *.dvi *.lof *.ps *.toc *.tmp *.blg
 latex thesis
+bibtex thesis
 latex thesis
 dvips thesis
 "c:\program files (x86)\miktex 2.8\miktex\bin\ps2pdf.exe" thesis.ps
