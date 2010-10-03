ea7cf7514a4f6e599083e28c29387cd0ad7c9690
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-10-03 14:50:50 +0000

    extract triangleStats

--- trunk/thesis/matlab/plot/plotAnchorTriangleHeightVsError.m
+++ trunk/thesis/matlab/plot/plotAnchorTriangleHeightVsError.m
@@ -7,26 +7,9 @@ maxRadius=results(end).radius;
 network=results(1).network;
 numAnchorSets=size(anchors,1);
 
-heights=zeros(numAnchorSets,4);
-for s=1:numAnchorSets
-    anchorNodes=anchors(s,:);
-    d=zeros(3,1);
-    d(1)=network.distanceMatrix(anchorNodes(1),anchorNodes(2));
-    d(2)=network.distanceMatrix(anchorNodes(2),anchorNodes(3));
-    d(3)=network.distanceMatrix(anchorNodes(3),anchorNodes(1));
-    A=heron(d(1),d(2),d(3));
-    h=zeros(3,1);
-    h(1)=2*A/d(2);
-    h(2)=2*A/d(3);
-    h(3)=2*A/d(1);
-    heights(s,1)=max(h);
-    heights(s,2)=median(h);
-    heights(s,3)=min(h);
-    heights(s,4)=sum(h);
-end
+heights=triangleStats(network.distanceMatrix,anchors).heights;
 
-
-figure('Name','Anchor Triangle Height vs Error'); %,'visible','off');
+figure('Name','Anchor Triangle Height vs Error','visible','off');
 plotTitle=sprintf('Network %s',strrep(network.shape,'-',' '));
 title({'Height of Triangle made by Anchors vs Localization Error',...
     plotTitle});
@@ -45,11 +28,16 @@ for r=1:size(results,2)
 
     hStddev=zeros(4,1);
     hRanges=zeros(4,1);
-    for i=1:4
-        hStddev(i)=std(heights(:,i));
-        hRanges(i)=range(heights(:,i));
-        fprintf(1,'Std Dev of %s: %.2f; Range:%.2f\n',stats{i},hStddev(i),hRanges(i));
-    end
+    
+    hStddev(1)=std(heights(:).max);
+    hRanges(1)=range(heights(:).max);
+    hRanges(2)=range(heights(:).median);
+    hRanges(2)=range(heights(:).median);
+    hRanges(3)=range(heights(:).min);
+    hRanges(3)=range(heights(:).min);
+    hRanges(4)=range(heights(:).sum);
+    hRanges(4)=range(heights(:).sum);
+    
     [m,index]=min(hStddev./hRanges);
     for i=index:index
         heights=heights./network.radius;
@@ -71,8 +59,3 @@ saveFigure(folder,filename);
 
 end
 
-%% Heron's Area of a Triangle formula
-function [triangleArea] = heron(a,b,c)
-s=(a+b+c)/2;
-triangleArea=sqrt(s*(s-a)*(s-b)*(s-c));
-end
