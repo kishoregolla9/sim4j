daa2ca4ca7b4d6ae0300b6d91050d758a080a3f3
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-10-03 02:07:23 +0000

    add svg figures

--- thesis/matlab/plot/saveFigure.m
+++ thesis/matlab/plot/saveFigure.m
@@ -14,7 +14,12 @@ if exist(pathstr,'file')==0
     [a,b]=mkdir(pathstr); %#ok<NASGU>
 end
 fprintf(1,'Saving %s\n',filename);
-print('-dpng',filename);
+print(h,'-dpng',filename);
+
+addpath('plot2svg_20100306');
+
+filename=strrep(filename,'.png','.svg');
+plot2svg(filename, h, 'png'); 
 
 filename=sprintf('%s/eps/%s.eps',folder,name);
 [pathstr]=fileparts(filename);
@@ -22,4 +27,5 @@ if exist(pathstr,'file')==0
     [a,b]=mkdir(pathstr); %#ok<NASGU>
 end
 fprintf(1,'Saving %s\n',filename);
-print('-depsc',filename);
+print(h, '-depsc',filename);
+
