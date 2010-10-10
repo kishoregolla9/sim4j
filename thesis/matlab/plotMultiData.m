a11f16123c3704ee8b909b7bedef017b71ae3c4d
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-10-10 00:55:44 +0000

    plot with/without exluding outliers, and add correlation coefficient

--- thesis/matlab/plotMultiData.m
+++ thesis/matlab/plotMultiData.m
@@ -1,8 +1,14 @@
 %% Plot Multiple Simulation Data vs max/mean errors
 function []=plotMultiData(folderAll,shapeLabel,description,...
-    numNetworks,numAnchorSets,maxErrors,meanErrors,data)
+    numNetworks,numAnchorSets,maxErrors,meanErrors,data,...
+    threshold,doConfidenceIntervals)
 
-threshold=100;
+if (exist('doConfidenceIntervals','var')==0)
+    doConfidenceIntervals=false;
+end
+if (exist('threshold','var')==0)
+    threshold=100;
+end
 
 networkconstants;
 % run(ccaconfigfile);
@@ -25,8 +31,6 @@ t=sprintf('%s\n%i networks, %i anchor sets\n%s %s',...
 title(t);
 
 errors=meanErrors;
-
-% Make bins based on the sample size of all x data
 numSamples=size(data,1)*size(data,2);
 x=zeros(numSamples,1);
 y=zeros(numSamples,1);
@@ -39,34 +43,47 @@ for i=1:size(data,1)
     end
 end
 
-% plot the actual y points (under the error bars)
+if (threshold ~= 100)
+    %Remove points where error > threshold times the radio range
+   outliers=find(y>threshold);
+   x(outliers)=[]; 
+   y(outliers)=[];
+end
+
+% plot the actual y points
 plot(x,y,'.');
 
-sortable=[x,y];
-d=sortrows(sortable);
-x=d(:,1);
-y=d(:,2);
+r=corrcoef(x,y);
+coef=sprintf('Correlation Coefficient: %.2f',r(1,2));
+bottomlabel=sprintf('%s\n%s',description,coef);
 
-numBins=20;
-pointsPerBin=size(x,1)/numBins;
-bx=zeros(pointsPerBin,numBins);
-by=zeros(pointsPerBin,numBins);
-index=1;
-for b=1:numBins
-    for j=1:pointsPerBin
-        bx(b,j)=x(index);
-        by(b,j)=y(index);
-        index=index+1;
+if (doConfidenceIntervals)
+    sortable=[x,y];
+    d=sortrows(sortable);
+    x=d(:,1);
+    y=d(:,2);
+    
+    % Make bins based on the sample size of all x data
+    numBins=20;
+    pointsPerBin=size(x,1)/numBins;
+    bx=zeros(pointsPerBin,numBins);
+    by=zeros(pointsPerBin,numBins);
+    index=1;
+    for b=1:numBins
+        for j=1:pointsPerBin
+            bx(b,j)=x(index);
+            by(b,j)=y(index);
+            index=index+1;
+        end
+    end
+    
+    for b=1:numBins
+        [ci]=getConfidenceInterval(confidence,by(b,:));
+        mu=mean(by(b,:));
+        errorbar(mean(bx(b,:)),mu,ci,'o','LineWidth',1.5);
     end
 end
-
-for b=1:numBins
-    [ci]=getConfidenceInterval(confidence,by(b,:));
-    mu=mean(by(b,:));
-    errorbar(mean(bx(b,:)),mu,ci,'o','LineWidth',1.5);
-end
-
-xlabel(description);
+xlabel(bottomlabel);
 ylabel('Location Error');
 numAnchorSets %#ok<NOPRT>
 % axis( [0 size(maxErrors,1) 0 1] )
