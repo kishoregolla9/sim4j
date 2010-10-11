b330512af7243c38389ded88549d90862e622db2
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-10-11 14:46:39 +0000

    big run config

--- thesis/matlab/ccaconfig.m
+++ thesis/matlab/ccaconfig.m
@@ -1,6 +1,6 @@
 
 % For multisimcca
-numNetworks=10 %#ok<NOPTS>
+numNetworks=25 %#ok<NOPTS>
 
 % For simcca
 numAnchorSets=100 %#ok<NOPTS>
@@ -8,7 +8,7 @@ numAnchorsPerSet=3 %#ok<NOPTS>
 doAllStarts=false;
 shape=NET.SHAPE_SQUARE;
 placement=NET.NODE_RANDOM;
-networkEdge=5%#ok<NOPTS>
+networkEdge=15 %#ok<NOPTS>
 networkHeight=networkEdge;
 networkWidth=networkEdge;
 numNodes=networkHeight*networkWidth; 
