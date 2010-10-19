9e0f2d8bd3b0344d1d1967f7d08292dcfff466b1
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-10-19 01:07:24 +0000

    more plotting generality

--- thesis/matlab/plot/plotAnchorDistanceVsError.m
+++ thesis/matlab/plot/plotAnchorDistanceVsError.m
@@ -9,25 +9,31 @@ numAnchorSets=size(anchors,1);
 
 %% Plot by Perimeter
 figName='Anchor Distance vs Error';
-dataName='Sum of Distance between Anchors';
+dataName='Distance between Anchors';
+dataLabels={'Sum' 'Minimum' 'Mean'};
 
-distances=zeros(1,numAnchorSets,1);
+distances=zeros(1,numAnchorSets,3);
 for s=1:numAnchorSets
     anchorNodes=anchors(s,:);
-    numAnchors=size(anchorNodes,2);
-    d=zeros(numAnchors*2-1,1);
+%     numAnchors=size(anchorNodes,2);
+    d=zeros(3,1);
     d(1)=network.distanceMatrix(anchorNodes(1),anchorNodes(2));
     d(2)=network.distanceMatrix(anchorNodes(2),anchorNodes(3));
     d(3)=network.distanceMatrix(anchorNodes(3),anchorNodes(1));
     distances(1,s,1)=sum(d);
+    distances(1,s,2)=min(d);
+    distances(1,s,3)=mean(d);
 end
 
 if (exist('threshold','var')==0)
-    plotSingleDataSet(figName,dataName,results,anchors,radii,distances,...
-        folder);
-else
-    plotSingleDataSet(figName,dataName,results,anchors,radii,distances,...
-        folder,threshold);
+    threshold=100;
+end
+
+for i=1:size(dataLabels,2)
+    f=sprintf('%s %s',dataLabels{i}, figName);
+    plotSingleDataSet(f,dataName,results,anchors,radii,...
+        distances(:,:,i),...
+        folder,threshold,{ dataLabels{i} });
 end
 
 end
--- thesis/matlab/plot/plotSingleDataSet.m
+++ thesis/matlab/plot/plotSingleDataSet.m
@@ -1,5 +1,5 @@
 function plotSingleDataSet( figName,dataName,...
-    results,anchors,radii,allData,folder,threshold)
+    results,anchors,radii,allData,folder,threshold,dataLabels)
 % Plot number of unique anchor neighbors (unique among all anchors in the
 % set) vs location error
 
@@ -22,35 +22,65 @@ end
 title(plotTitle);
 hold all
 grid on
-labels=cell(1, numRadii);
+
+if iscell(allData)
+    numData=size(allData{1},2);
+else
+    numData=size(allData,3);
+end
+
+labels=cell(1, numRadii * numData);
+p=1;
 for r=1:numRadii
-    data=allData(r,:,:)';
-    errorPerAnchorSet=zeros(numAnchorSets,1);
+    y=zeros(numAnchorSets,1);
     for s=1:numAnchorSets
         % For one start node
-        errorPerAnchorSet(s)=[results(r).errors(s,1).mean];
+        y(s)=[results(r).errors(s,1).mean];
     end
+
+    outliers=find(y>threshold);
+    y(outliers)=[];
     
-    % Remove outliers greater than threshold
-    outliers=find(errorPerAnchorSet>threshold);
-    errorPerAnchorSet(outliers)=[];
-    data(outliers)=[];
+    markers={ 'o' 'v' 'd' };
     
-    dataToPlot=[data, errorPerAnchorSet];
-    dataToPlot=sortrows(dataToPlot,1);
-    plot(dataToPlot(:,1),dataToPlot(:,2),'ok');
-    
-    p = polyfit(data, errorPerAnchorSet, 2);
-    Output = polyval(p,data);
-    correlation = corrcoef(errorPerAnchorSet, Output);
-    
-    if (numRadii > 1)
-        labels{r}=sprintf('Radius=%.1f,Correlation=%.2f',...
-            results(r).radius,correlation(1,2));
+    for d=1:numData
+        if iscell(allData)
+            x=allData{r};
+            x=x(:,d);
+        else
+            x=allData(r,:,d)';
+        end
+        
+        % Remove outliers greater than threshold
+        x(outliers)=[];
+        
+        dataToPlot=[x, y];
+        dataToPlot=sortrows(dataToPlot,1);
+        ls=sprintf('%sk',markers{d});
+        plot(dataToPlot(:,1),dataToPlot(:,2),ls);
+        
+        fit = polyfit(x, y, 2);
+        Output = polyval(fit,x);
+        correlation = corrcoef(y, Output);
+        
+        if (exist('dataLabels','var') == 1)
+            lab=sprintf('%s - Correlation=%.2f',...
+                dataLabels{d},correlation(1,2));
+            if (numRadii > 1)
+                lab=sprintf('%s - Radius=%.1f',lab,results(r).radius);
+            end
+            labels{p}=lab;
+        elseif (numRadii > 1)
+            lab=sprintf('%s  - Radius=%.1f - Correlation=%.2f',...
+                results(r).radius,correlation(1,2));
+            labels{p}=lab;
+        end
+        p=p+1;
     end
 end
-%legend(labels,'Location','NorthEast');
-if (numRadii == 1)
+
+legend(labels,'Location','NorthEast');
+if (numRadii == 1 && exist('dataLabels','var')==0)
     dataName=sprintf('%s\nCorrelation Coefficient=%.2f',...
         dataName,correlation(1,2));
 end
