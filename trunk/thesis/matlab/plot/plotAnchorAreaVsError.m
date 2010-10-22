a778e3b1693617bf35b2585f8c22d018e8338249
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-10-22 02:04:00 +0000

    more plotting

--- trunk/thesis/matlab/plot/plotAnchorAreaVsError.m
+++ trunk/thesis/matlab/plot/plotAnchorAreaVsError.m
@@ -1,48 +1,20 @@
-function plotAnchorAreaVsError( results,anchors,folder )
+function plotAnchorAreaVsError( results,anchors,radii,folder,threshold )
 % Plot distance between the anchors themselves vs error
-
-minRadius=results(1).radius;
-maxRadius=results(end).radius;
-
 network=results(1).network;
-numAnchorSets=size(anchors,1);
 
 %% Plot by Area
 
+figName='Anchor Area vs Error';
+dataName='Area of Anchor Triangle';    
+
 stats=triangleStats(network,anchors);
 
-hold off
-fig=figure('Name','Anchor Triangle Area vs Error','visible','off');
-plotTitle=sprintf('Network %s',network.shape);
-grid on
-labels=cell(1, size(results,2));
-title({'Area of Triangle made by Anchors vs Localization Error',...
-    plotTitle});
-for r=1:size(results,2)
-    errorPerAnchorSet=zeros(numAnchorSets,1);
-    for s=1:numAnchorSets
-        % For one start node
-        errorPerAnchorSet(s)=[results(r).errors(s,1).mean];
-    end    
-    
-    dataToPlot=[stats.areas, stats.distancesToCentroid, errorPerAnchorSet];
-    dataToPlot=sortrows(dataToPlot,1);    
-    scatter(dataToPlot(:,1),dataToPlot(:,3),... % X,Y of circles
-         5,...              % size of circles
-         dataToPlot(:,2));  % color of circles
-    grid on
-    labels{r}=sprintf('Radius=%.1f',results(r).radius);
+if (exist('threshold','var')==0)
+    plotSingleDataSet(figName,dataName,results,anchors,radii,stats.areas',...
+        folder);
+else
+    plotSingleDataSet(figName,dataName,results,anchors,radii,stats.areas',...
+        folder,threshold);
 end
-legend(labels,'Location','Best');
-xlabel({'Area of Triangle Anchors',...
-    'Color corresponds to distance of centroid to centre of networks'});
-ylabel('Mean Location Error');
-axes1=get(fig,'CurrentAxes');
-set(axes1,'XScale','log');
-hold off
-
-filename=sprintf('AnchorTriangleAreaVsError-%s-Radius%.1f-to-%.1f',...
-    network.shape,minRadius,maxRadius);
-saveFigure(folder,filename);
 
 end
