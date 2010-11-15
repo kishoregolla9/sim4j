84c24c4978a1b6dbaca69e92dad069a14d2876e2
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-11-15 23:28:55 +0000

    contours

---
+++ trunk/thesis/matlab/network/plotContours.m
@@ -0,0 +1,25 @@
+function[]=plotContours(x,y,z,cmap,a)
+
+if (~exist('a','var'))
+   a=1; 
+end
+
+% Determine the minimum and the maximum x and y values:
+xmin = min(x); ymin = min(y);
+xmax = max(x); ymax = max(y);
+% Define the resolution of the grid:
+res=200;
+% Define the range and spacing of the x- and y-coordinates,
+% and then fit them into X and Y
+xv = linspace(xmin, xmax, res);
+yv = linspace(ymin, ymax, res);
+[Xinterp,Yinterp] = meshgrid(xv,yv);
+% Calculate Z in the X-Y interpolation space, which is an
+% evenly spaced grid:
+Zinterp = griddata(x,y,z,Xinterp,Yinterp);
+% Generate the mesh plot (CONTOUR can also be used):
+colormap(cmap);
+contourf(Xinterp,Yinterp,Zinterp,'k-');
+% alpha(a);
+colorbar('location','southoutside')
+end
