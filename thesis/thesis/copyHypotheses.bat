e20adb973f6c23a169b1971b1bac33eb7c30e754
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-11-15 18:33:41 +0000

    section 4.2

--- thesis/thesis/copyHypotheses.bat
+++ thesis/thesis/copyHypotheses.bat
@@ -1,8 +1,8 @@
 copy %1\eps\Anchor_Neighbors_vs_Error-Random-20x20-Square-with-400-nodes-Radius2.5-to-2.5.eps ..\thesis\figures\numneighbors\AnchorNeighborsVsError.eps
 copy %1\eps\Anchor_Neighbors_vs_Error-Random-20x20-Square-with-400-nodes-Radius2.5-to-2.5-Excluding2.0.eps ..\thesis\figures\numneighbors\AnchorNeighborsVsErrorExcluding.eps
 
-copy %1\eps\AnchorSetErrors-Random-20x20-Square-with-400-nodes-Radius2.5.eps ..\thesis\figures\numneighbors\AnchorSetErrorVsError.eps
-copy %1\eps\AnchorSetErrors-Random-20x20-Square-with-400-nodes-Radius2.5-Excluding2.0.eps ..\thesis\figures\numneighbors\AnchorSetErrorVsErrorExcluding.eps
+copy %1\eps\AnchorSetErrors-Random-20x20-Square-with-400-nodes-Radius2.5-to-2.5.eps ..\thesis\figures\numneighbors\AnchorSetErrorVsError.eps
+copy %1\eps\AnchorSetErrors-Random-20x20-Square-with-400-nodes-Radius2.5-to-2.5-Excluding2.0.eps ..\thesis\figures\numneighbors\AnchorSetErrorVsErrorExcluding.eps
 
 for %%s in ( Sum Minimum Mean ) do (
   copy %1\eps\%%s_Anchor_Distance_vs_Error-Random-20x20-Square-with-400-nodes-Radius2.5-to-2.5.eps ..\thesis\figures\distances\%%sAnchorDistanceVsError.eps
