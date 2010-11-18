3ae9464b70926ddd050fdc95c333b55fa105e30e
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-11-18 20:11:54 +0000

    more plots

--- thesis/matlab/indicator.m
+++ thesis/matlab/indicator.m
@@ -1,25 +1,52 @@
 
-folders={...    
+randomsquare={...
     '2010-11-16_14_25_47-Square-Random-20x20',...
-    '2010-11-16_14_7_17-Square-Random-20x20'
+    '2010-11-16_14_7_17-Square-Random-20x20',...
+    '2010-11-18_9_15_24-Square-Random-20x20',...
+    '2010-11-18_9_31_50-Square-Random-20x20',...
+    '2010-11-18_9_48_47-Square-Random-20x20',...
+    '2010-11-18_10_7_43-Square-Random-20x20',...
+    '2010-11-18_10_25_32-Square-Random-20x20'
+    };
+
+cfolders={...
+    
 };
 
-errors=zeros(0,1);
+folders=cfolders;
+
+allErrors=zeros(0,1);
 heights=zeros(0,1);
 for i=1:length(folders)
-   f=sprintf('../results/%s',folders{i});
-   loadFile(f,'result-*');
-   loadFile(f,'anchors*');
-   errors=[ errors [result.errors.mean] ];
-   stats=triangleStats(result.network.points,anchors,result.network.width,result.network.height);
-   heights=[ heights [stats.heights.min] ];
-   clear result anchors numAnchorSets
+    folder=sprintf('../results/%s',folders{i});
+    vars=loadFile(folder,'errorsAndAnchors');
+    if (isempty(vars))
+        vars=loadFile(folder,'result');
+        result=vars.result;
+        errors=result.errors;
+        network=result.network;
+        radius=result.radius;
+        vars=loadFile(folder,'anchors');
+        anchors=vars.anchors;
+        
+        savefile=sprintf('%s/errorsAndAnchors.mat',folder);
+        save(savefile,'errors','anchors','network','radius');
+    else
+        errors=vars.errors;
+        network=vars.network;
+        anchors=vars.anchors;
+        if (isfield(vars,'radius'))
+            radius=vars.radius;
+        else
+            radius=2.5;
+        end
+    end
+    allErrors=[ allErrors [errors.mean] ]; %#ok grow
+    stats=triangleStats(network.points,anchors,network.width,network.height);
+    heights=[ heights [stats.heights.min] ]; %#ok grow
+    %     clear network errors anchors numAnchorSets
 end
-radius=radii(1);
-plotIndicators(errors,heights,radius);
-
-
-
-
-
+h=plotIndicators(allErrors,heights,radius);
+filename=sprintf('HeightIndicator_%04i%02i%02i_%02i%02i%02i',fix(clock));
+saveFigure('..',filename,h);
 
---
+++ thesis/matlab/plotSampleAnchors.m
@@ -0,0 +1,58 @@
+
+s=428;
+
+anchorNodes=anchors(s,:);
+
+patchedMaps=result.patchedMap;
+transformedPoints=patchedMaps(s).mappedPoints;
+realPoints=network.points;
+
+temp=sprintf('%s/patchedMaps',folder);
+vars=loadFile(temp,'patchedMap');
+patchedPoints=vars.rawResult;
+clear vars
+
+realAnchors=realPoints(anchorNodes,:);
+transformedAnchors=transformedPoints(anchorNodes,:);
+patchedAnchors=patchedPoints(anchorNodes,:);
+
+h=figure('Name','Sample Anchor Transformation');
+grid on
+hold all;
+% for i=1:size(realAnchors,1)
+%     pa=plot([realAnchors(i,1),transformedAnchors(i,1)],...
+%         [realAnchors(i,2),transformedAnchors(i,2)],'-k','MarkerSize',3);
+% end
+% labels{1} = '';
+
+% Overlay the real points with diamonds
+% to distinguish them from the transformed points
+% pb=plot(realAnchors(:,1),realAnchors(:,2),'dk','MarkerSize',6);
+% labels{1} = 'Real Points';
+
+% Overlay the transformed points with circles
+% to distinguish them from the transformed points
+% pc=plot(transformedAnchors(:,1),transformedAnchors(:,2),'ok','MarkerSize',6);
+% labels{end+1} = 'transformed Points';
+
+% Show a circle of the radius around each anchor point
+% and Draw the Anchor Triangle
+plotAnchorTriangle(anchorNodes,patchedPoints,0,'','^','--',':');
+labels{1} = 'Patched Anchors';
+plotAnchorTriangle(anchorNodes,realPoints,0,'','d','-','-');
+labels{2} = 'Real Anchors';
+plotAnchorTriangle(anchorNodes,transformedPoints,0,'','o',':',':');
+labels{3} = 'Transformed Anchors';
+
+% xlim([0 network.width]);
+% ylim([0 network.height]);
+legend(labels);
+clear labels
+
+
+[d, a, transform]=procrustes(realPoints, transformedPoints);
+transform.T;
+
+% filename=sprintf('SampleAnchors_%04i%02i%02i_%02i%02i%02i',fix(clock));
+saveFigure('..','SampleAnchors',h);
+
