3dbc64fce6d9e7080025346744c1bedf506bc68c
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-11-21 19:51:04 +0000

    run many

--- thesis/matlab/ccaconfig.m
+++ thesis/matlab/ccaconfig.m
@@ -1,14 +1,14 @@
-
 % For multisimcca
-numNetworks=25 %#ok<NOPTS>
+numNetworks=10 %#ok<NOPTS>
+% folderpath='../results/multi-20101018_213558_Square-Random-12x12-100Sets-10Nets'
 
 % For simcca
-numAnchorSets=100 %#ok<NOPTS>
+numAnchorSets=5000 %#ok<NOPTS>
 numAnchorsPerSet=3 %#ok<NOPTS>
 doAllStarts=false;
 shape=NET.SHAPE_RECTANGLE;
 placement=NET.NODE_RANDOM;
-networkEdge=15;
-networkHeight=2 %#ok
-networkWidth=100 %#ok
+networkEdge=20 %#ok<NOPTS>
+networkHeight=networkEdge;
+networkWidth=networkEdge;
 numNodes=networkHeight*networkWidth; 
