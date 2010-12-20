ed938de5840a9640db930493ee1f652849fb589b
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2010-12-20 05:13:45 +0000

    pipelines

--- trunk/thesis/matlab/plotIndicators.m
+++ trunk/thesis/matlab/plotIndicators.m
@@ -1,31 +1,33 @@
-function [h]=plotIndicators(errors,stat,radius,label,increment,showOutliers,xscale)
+function [h]=plotIndicators(errors,stat,radius,label,increment,showOutliers,xscale,names,connectivityLevels)
 % errors cell array of arrays of errors
 % stat cell array (same size as errors)
 %
 
 h=figure('Name','Outlier Indicator','visible','off');
-m={'g.','co','rs'};
-   
+m={'g.','co','rs','b^'};
+numsubs=max(1,length(errors)/2);
 for e=1:length(errors)
+    if(numsubs>1)
+        subplot(numsubs,2,e);
+    end
     dataToPlot=[stat{e}/radius; errors{e}]';
     dataToPlot=sortrows(dataToPlot,1);
     x=dataToPlot(:,1);
     y=dataToPlot(:,2);
-    plot(x,y,m{e});
+    plot(x,y,m{1},'HandleVisibility','off');
     hold all
-end    
+% end    
 outlierSep=2.5;
 confidence=0.05;
-for e=1:length(errors)
+% for e=1:length(errors)
     % plot(x,y,'.');
     hold all
 
-    dataToPlot=[stat{e}/radius; errors{e}]';
-    dataToPlot=sortrows(dataToPlot,1);
-    x=dataToPlot(:,1);
-    y=dataToPlot(:,2);
+%     dataToPlot=[stat{e}/radius; errors{e}]';
+%     dataToPlot=sortrows(dataToPlot,1);
+%     x=dataToPlot(:,1);
+%     y=dataToPlot(:,2);
 
-    
     grid on
     set(gca,'XScale',xscale);
     xlabel(label);
@@ -46,30 +48,39 @@ for e=1:length(errors)
         mu(i)=mean(v);
         %     plot(repmat(bins(i),length(v),1),v,'g.');
         d=bins(i)-(increment/2);
-        errorbar(d,mu(i),ci,'.k');
+        errorbar(d,mu(i),ci,'.k','HandleVisibility','off');
         d=d-(increment/2);
         if (bottom == 0 && strcmp(xscale,'log'))
             bottom=0.0001;
         end
-        line([bottom top], [ mu(i) mu(i) ],'LineStyle','-','LineWidth',1,'Color','k');
+        line([bottom top], [ mu(i) mu(i) ],'LineStyle','-','LineWidth',1,'Color','k','HandleVisibility','off');
         if (showOutliers && ~crossed && ~isempty(v) &&  max(v) < outlierSep)
             crossed=true;
-            line([d d],[0 7],'LineStyle','--','LineWidth',2);
+            line([d d],[0 7],'LineStyle','--','LineWidth',2,'HandleVisibility','off');
             lastoutlier=sprintf('\\leftarrowLast Outlier %.1fr',d);
-            text(d,6.5,lastoutlier,'Color','k','FontSize',14);
+            text(d,6.5,lastoutlier,'Color','k','FontSize',14,'HandleVisibility','off');
         end
         
     end
     
     if (showOutliers)
-        line([0.0001 d],[outlierSep + 0.2 outlierSep + 0.2],'LineStyle','--','LineWidth',2,'Color','k');
-        text(d/4,2.5,'Outlier separator');
+        line([0.0001 d],[outlierSep + 0.2 outlierSep + 0.2],'LineStyle','--','LineWidth',2,'Color','k','HandleVisibility','off');
+        text(d/4,2.5,'Outlier separator','HandleVisibility','off');
     end
     
     l=ylim;
     ylim([0 l(2)])
     
     % plot a line through the mean points
-    plot(bins(2:length(bins))-(increment/2),mu(2:length(bins)),'-k');
+    s={'-k',':k','-.k','--k'};
+    dn=sprintf('%s, CL=%.2f',names{e},connectivityLevels(e));
+    plot(bins(2:length(bins))-(increment/2),mu(2:length(bins)),s{1},'DisplayName',names{e});
+    if ~showOutliers
+        ylim([0 3]);
+    end
+    xlim([0.0001 10]);
+    title(dn);
+%     legend('show')
 end
+
 end
