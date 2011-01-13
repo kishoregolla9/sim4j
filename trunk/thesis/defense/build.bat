fde37a76f6b522c42994f6518edd1875e2caedbe
bentatham <bentatham@05e6dff8-d244-0410-89c8-217543e244fc>
2011-01-13 02:34:57 +0000

    add itemize and columns

--- trunk/thesis/defense/build.bat
+++ trunk/thesis/defense/build.bat
@@ -1,7 +1,5 @@
 del *.aux *.dvi *.lof *.ps *.toc *.tmp *.blg
 latex defense
-bibtex defense
-latex defense
 dvips defense
 "c:\program files (x86)\miktex 2.8\miktex\bin\ps2pdf.exe" defense.ps
 start defense.pdf
